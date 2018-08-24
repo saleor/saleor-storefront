b3ffa72f0bf9855ea880d4c048a86b041d8a5719
Michał Ociepka <michal@mirumee.com>
2018-08-24 14:43:18 +0200

    Reformat all ts files

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,25 +1,29 @@
-import { Base64 } from 'js-base64';
+import { Base64 } from "js-base64";
 
-export const slugify = (text: string | number):string => (
-    text.toString().toLowerCase().trim()
-    .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/&/g, '-and-')         // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-)
+export const slugify = (text: string | number): string =>
+  text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
 
-export const getDBIdFromGraphqlId = (graphqlId: string, schema?: string): number => {
-    // This is temporary solution, we will use slugs in the future
-    const rawId = Base64.decode(graphqlId);
-    const regexp = /(\w+):(\d+)/;
-    const [, expectedSchema, id] = regexp.exec(rawId);
-    if (schema && schema !== expectedSchema) {
-        throw "Schema is not correct";
-    }
-    return parseInt(id, 10);
-}
+export const getDBIdFromGraphqlId = (
+  graphqlId: string,
+  schema?: string
+): number => {
+  // This is temporary solution, we will use slugs in the future
+  const rawId = Base64.decode(graphqlId);
+  const regexp = /(\w+):(\d+)/;
+  const [, expectedSchema, id] = regexp.exec(rawId);
+  if (schema && schema !== expectedSchema) {
+    throw new Error("Schema is not correct");
+  }
+  return parseInt(id, 10);
+};
 
-export const getGraphqlIdFromDBId = (id: string, schema: string): string => (
-    // This is temporary solution, we will use slugs in the future
-    Base64.encode(`${schema}:${id}`)
-)
+export const getGraphqlIdFromDBId = (id: string, schema: string): string =>
+  // This is temporary solution, we will use slugs in the future
+  Base64.encode(`${schema}:${id}`);
