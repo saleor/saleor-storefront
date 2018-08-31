7f714828f7dc0b2b9f819d224610d1d97984ff8b
Michał Ociepka <michal@mirumee.com>
2018-08-31 10:33:07 +0200

    Use basic product fields fragment

--- src/components/ProductPage/queries.ts
+++ src/components/ProductPage/queries.ts
@@ -1,12 +1,44 @@
 import gql from "graphql-tag";
 
+export const BASIC_PRODUCT_FRAGMENT = gql`
+  fragment BasicProductFields on Product {
+    id
+    name
+    thumbnailUrl
+  }
+`;
+
 export const GET_PRODUCT_DETAILS = gql`
+  ${BASIC_PRODUCT_FRAGMENT}
   query ProductDetails($id: ID!) {
     product(id: $id) {
-      id
-      name
+      ...BasicProductFields
       description
-      thumbnailUrl
+      variants {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_PRODUCT_VARIANT_DETAILS = gql`
+  ${BASIC_PRODUCT_FRAGMENT}
+  query ProductVariantDetails($id: ID!) {
+    productVariant(id: $id) {
+      id
+      stockQuantity
+      costPrice {
+        currency
+        amount
+      }
+      product {
+        ...BasicProductFields
+      }
     }
   }
 `;
