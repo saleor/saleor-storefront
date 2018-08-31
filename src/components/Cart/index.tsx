f9e3c541ddf9fa1c80ebad3cfbac9274a82ae428
Michał Ociepka <michal@mirumee.com>
2018-08-31 10:18:50 +0200

    Add cart provider

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { Query } from "react-apollo";
+import { OverlayContext, OverlayType } from "../App/context";
+import Overlay from "../Overlay";
+import { CartContext, CartInterface, CartLineInterface } from "./context";
+
+import { GET_VARIANTS } from "./queries";
+
+export class CartProvider extends React.Component<
+  { children: any },
+  CartInterface
+> {
+  constructor(props) {
+    super(props);
+    let lines;
+    try {
+      lines = JSON.parse(localStorage.getItem("cart")) || [];
+    } catch {
+      lines = [];
+    }
+    this.state = {
+      add: this.add,
+      changeQuantity: this.changeQuantity,
+      clear: this.clear,
+      getQuantity: this.getQuantity,
+      lines,
+      remove: this.remove
+    };
+  }
+
+  changeQuantity = (variantId, quantity) => {
+    const newLine: CartLineInterface = {
+      quantity,
+      variantId
+    };
+    this.setState(prevState => {
+      let lines = prevState.lines.filter(line => line.variantId !== variantId);
+      if (newLine.quantity > 0) {
+        lines = [...lines, newLine];
+      }
+      return { lines };
+    });
+  };
+
+  add = (variantId, quantity = 1) => {
+    const line = this.state.lines.find(line => line.variantId === variantId);
+    const newQuantity = line ? line.quantity + quantity : quantity;
+    this.changeQuantity(variantId, newQuantity);
+  };
+
+  clear = () => this.setState({ lines: [] });
+
+  getQuantity = () =>
+    this.state.lines.reduce((sum, line) => sum + line.quantity, 0);
+
+  remove = variantId => this.changeQuantity(variantId, 0);
+
+  componentDidUpdate(prevProps, prevState) {
+    localStorage.setItem("cart", JSON.stringify(this.state.lines));
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <CartContext.Provider value={this.state}>{children}</CartContext.Provider>
+    );
+  }
+}
