6c185afd47b9a0af41a6f6cab4f0c57b62652104
Michał Ociepka <michal@mirumee.com>
2018-09-17 19:08:16 +0200

    Extend TextField component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
+import { FormError } from "../Form";
+
 import "./scss/index.scss";
 
 interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  error?: string;
+  errors?: FormError[];
   helpText?: string;
   label?: string;
   iconLeft?: React.ReactNode;
@@ -14,7 +16,7 @@ const TextField: React.SFC<TextFieldProps> = ({
   label = "",
   iconLeft,
   iconRight,
-  error,
+  errors,
   helpText,
   ...rest
 }) => (
@@ -24,11 +26,15 @@ const TextField: React.SFC<TextFieldProps> = ({
     {iconRight ? <span className="input__icon-right">{iconRight}</span> : null}
     <input
       {...rest}
-      className={`input__field${error ? " input__field--error" : ""}${
-        iconLeft ? " input__field--left-icon" : ""
-      }`}
+      className={`input__field${
+        errors && errors.length ? " input__field--error" : ""
+      }${iconLeft ? " input__field--left-icon" : ""}`}
     />
-    {error && <span className="input__error">{error}</span>}
+    {errors && (
+      <span className="input__error">
+        {errors.map(error => error.message).join(" ")}
+      </span>
+    )}
     {helpText && <span className="input__help-text">{helpText}</span>}
   </div>
 );
