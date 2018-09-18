ded5a75a06f204e16451021487528512864e5047
Michał Ociepka <michal@mirumee.com>
2018-09-18 12:59:45 +0200

    Add user auth queries

---
+++ src/components/User/queries.ts
@@ -0,0 +1,38 @@
+import gql from "graphql-tag";
+
+export const USER_FRAGMENT = gql`
+  fragment User on User {
+    id
+    email
+    isStaff
+    note
+  }
+`;
+
+export const TOKEN_AUTH_MUTATION = gql`
+  ${USER_FRAGMENT}
+  mutation TokenAuth($email: String!, $password: String!) {
+    tokenCreate(email: $email, password: $password) {
+      token
+      errors {
+        field
+        message
+      }
+      user {
+        ...User
+      }
+    }
+  }
+`;
+
+export const TOKEN_VERIFICATION_MUTATION = gql`
+  ${USER_FRAGMENT}
+  mutation VerifyToken($token: String!) {
+    tokenVerify(token: $token) {
+      payload
+      user {
+        ...User
+      }
+    }
+  }
+`;
