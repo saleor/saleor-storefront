ff338de77c69ed2aecd6929e2ebfc3c69313672a
dominik-zeglen <flesz3@o2.pl>
2018-11-12 15:35:16 +0100

    Sort imports

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
 import classNames from "classnames";
+import * as React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 
 import "./scss/index.scss";
