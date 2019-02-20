ba3cf43da1734bd18bc8a9a7a213338c0db0ff08
Piotr Grundas <piotrgrundas@gmail.com>
2019-02-20 23:33:52 +0100

    Fix wrong automerge

--- src/checkout/index.tsx
+++ src/checkout/index.tsx
@@ -14,56 +14,45 @@ import {
 } from "../components";
 import { CartContext } from "../components/CartProvider/context";
 import { BASE_URL } from "../core/config";
-import { isPath } from "../core/utils";
 import { CheckoutContext } from "./context";
-import { orderConfirmationUrl, Routes } from "./routes";
+import { Routes } from "./routes";
 
 import logoImg from "../images/logo.svg";
 
-const CheckoutApp: React.FC<RouteComponentProps> = ({
-  history: {
-    location: { pathname }
-  }
-}) => {
-  const orderConfirmationPage = isPath(pathname, orderConfirmationUrl);
-
-  return (
-    <div className="checkout">
-      <div className="checkout__menu">
-        <div className="checkout__menu__bar">
-          <ReactSVG path={logoImg} />
-        </div>
-        {!orderConfirmationPage && (
-          <Link to={BASE_URL}>Return to shopping</Link>
-        )}
+const CheckoutApp: React.FC = () => (
+  <div className="checkout">
+    <div className="checkout__menu">
+      <div className="checkout__menu__bar">
+        <ReactSVG path={logoImg} />
       </div>
-      <div className="container">
-        <Online>
-          <CartContext.Consumer>
-            {cart => (
-              <CheckoutContext.Consumer>
-                {({ loading }) => {
-                  if (!cart.lines.length && !orderConfirmationPage) {
-                    return <Redirect to={BASE_URL} />;
-                  }
-
-                  if (loading) {
-                    return <Loader />;
-                  }
-
-                  return <Routes />;
-                }}
-              </CheckoutContext.Consumer>
-            )}
-          </CartContext.Consumer>
-        </Online>
-        <Offline>
-          <OfflinePlaceholder />
-        </Offline>
-      </div>
-      <OverlayManager />
+      <Link to={BASE_URL}>Return to shopping</Link>
     </div>
-  );
-};
+    <div className="container">
+      <Online>
+        <CartContext.Consumer>
+          {cart => (
+            <CheckoutContext.Consumer>
+              {({ loading }) => {
+                if (!cart.lines.length) {
+                  return <Redirect to={BASE_URL} />;
+                }
+
+                if (loading) {
+                  return <Loader />;
+                }
+
+                return <Routes />;
+              }}
+            </CheckoutContext.Consumer>
+          )}
+        </CartContext.Consumer>
+      </Online>
+      <Offline>
+        <OfflinePlaceholder />
+      </Offline>
+    </div>
+    <OverlayManager />
+  </div>
+);
 
 export default CheckoutApp;
