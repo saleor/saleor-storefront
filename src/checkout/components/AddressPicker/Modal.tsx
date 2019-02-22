14779685d25d4d1f556cc7777dfc6a86818e3d3f
Piotr Grundas <piotrgrundas@gmail.com>
2019-02-22 15:54:29 +0100

    Add ability to add new address

--- src/checkout/components/AddressPicker/Modal.tsx
+++ src/checkout/components/AddressPicker/Modal.tsx
@@ -1,30 +1,130 @@
 import * as React from "react";
 import ReactSVG from "react-svg";
 
-import { Button, ShippingAddressForm } from "../../../components";
-import { SUPPORT_EMAIL } from "../../../core/config";
+import { Button, Form, SelectField, TextField } from "../../../components";
+import { ShopContext } from "../../../components/ShopProvider/context";
 
 import closeImg from "../../../images/modal-close.svg";
+import { Address } from "../../types/Address";
 
-const ShippingUnavailableModal: React.FC<{ hide(): void }> = ({ hide }) => (
-  <div className="modal">
-    <div className="modal__title">
-      <p>We’re not shipping to your country</p>{" "}
-      <ReactSVG path={closeImg} className="modal__close" onClick={hide} />
-    </div>
-    <div className="modal__body">
-      <p className="modal__body__lead">We’re not shipping to your country</p>
-      <ShippingAddressForm
-        buttonText="Continue to Shipping"
-        onSubmit={() => null}
-      />
-    </div>
-    <div className="modal__footer">
-      <Button className="modal__button" onClick={hide}>
-        Ok
-      </Button>
-    </div>
-  </div>
+export interface NewAddressI extends Address {
+  email: string;
+}
+
+const AddNewAddressModal: React.FC<{
+  hide(): void;
+  onAddNew(address: Address, select: boolean): void;
+}> = ({ hide, onAddNew }) => (
+  <ShopContext.Consumer>
+    {({ countries, geolocalization, defaultCountry }) => (
+      <div className="modal">
+        <Form
+          data={{
+            country: {
+              code: geolocalization.country
+                ? geolocalization.country.code
+                : defaultCountry.code,
+              country: geolocalization.country
+                ? geolocalization.country.country
+                : defaultCountry.country
+            }
+          }}
+          onSubmit={(evt, data) => {
+            evt.preventDefault();
+            const { asNew, ...address } = data;
+            // TODO Fix types
+            onAddNew(address as any, asNew as any);
+            hide();
+          }}
+        >
+          <div className="modal__title">
+            <p>Add new address</p>
+            <ReactSVG path={closeImg} className="modal__close" onClick={hide} />
+          </div>
+          <div className="modal__body">
+            <TextField
+              label="Email Address"
+              type="email"
+              autoComplete="email"
+              name="email"
+            />
+            <TextField
+              label="Phone number"
+              type="tel"
+              name="phone"
+              autoComplete="tel"
+            />
+            <TextField
+              label="First Name"
+              type="given-name"
+              name="firstName"
+              autoComplete="given-name"
+            />
+            <TextField
+              label="Last Name"
+              type="family-name"
+              name="lastName"
+              autoComplete="family-name"
+            />
+            <TextField
+              label="Company"
+              type="organization"
+              name="companyName"
+              autoComplete="organization"
+            />
+            <TextField
+              label="Street Name"
+              type="address-line1"
+              name="streetAddress1"
+              autoComplete="address-line1"
+            />
+            <TextField
+              label="House / Apt Number"
+              type="address-line2"
+              name="streetAddress2"
+              autoComplete="address-line2"
+            />
+            <TextField
+              label="Zip-Code"
+              type="postal-code"
+              name="postalCode"
+              autoComplete="postal-code"
+            />
+            <TextField
+              label="City"
+              type="city"
+              name="city"
+              autoComplete="address-level2"
+            />
+            <TextField
+              label="State/Province"
+              type="state"
+              name="countryArea"
+              autoComplete="address-level1"
+            />
+            <SelectField
+              label="Country"
+              name="country"
+              options={countries.map(country => ({
+                label: country.country,
+                value: country.code
+              }))}
+            />
+            <label className="checkbox">
+              <input name="asNew" type="checkbox" />
+              <span>Use this address as new shipping address</span>
+            </label>
+          </div>
+          <div className="modal__footer">
+            <a>Cancel</a>
+            <Button type="submit" className="modal__button">
+              Ok
+            </Button>
+          </div>
+        </Form>
+      </div>
+    )}
+  </ShopContext.Consumer>
 );
 
-export default ShippingUnavailableModal;
+export default AddNewAddressModal;
--- src/checkout/components/AddressPicker/Picker.tsx
+++ src/checkout/components/AddressPicker/Picker.tsx
@@ -4,42 +4,57 @@ import classNames from "classnames";
 import React from "react";
 import ReactSVG from "react-svg";
 
+import { OverlayContext, OverlayTheme, OverlayType } from "../../../components";
 import AddressSummary from "../../../components/AddressSummary";
 import { Option } from "../../components";
 import { Address } from "../../types/Address";
+import AddNewAddressModal from "./Modal";
 
 import plusSvg from "../../../images/plus.svg";
 
 const Picker: React.FC<{
   addresses: Address[];
-  selectedId: string | null;
+  selectedAddress?: Address;
   onSelect(address: Address): void;
-  onAddNew?(address: Address): void;
-}> = ({ addresses, selectedId, onSelect, onAddNew }) => (
+  onAddNew?(address: Address, select: boolean): void;
+}> = ({ addresses, selectedAddress, onSelect, onAddNew }) => (
   <div className="address-picker">
-    {addresses.map(address => (
+    {addresses.map((address, id) => (
       <div
-        key={address.id}
+        key={id}
         onClick={() => onSelect(address)}
         className={classNames("address-picker__address", {
-          "address-picker__address--selected": selectedId === address.id
+          "address-picker__address--selected": selectedAddress === address
         })}
       >
         <AddressSummary address={address} />
         <Option
-          selected={selectedId === address.id}
-          value={`${address.id}`}
+          selected={selectedAddress === address}
+          value={`${id}`}
           label="Deliver to this address"
         />
       </div>
     ))}
     {onAddNew && (
-      <div className="address-picker__address address-picker__address--add-new">
-        <div>
-          <ReactSVG path={plusSvg} />
-          <p>Add new address</p>
-        </div>
-      </div>
+      <OverlayContext.Consumer>
+        {overlay => (
+          <div
+            className="address-picker__address address-picker__address--add-new"
+            onClick={() => {
+              overlay.show(OverlayType.modal, OverlayTheme.modal, {
+                content: (
+                  <AddNewAddressModal onAddNew={onAddNew} hide={overlay.hide} />
+                )
+              });
+            }}
+          >
+            <div>
+              <ReactSVG path={plusSvg} />
+              <p>Add new address</p>
+            </div>
+          </div>
+        )}
+      </OverlayContext.Consumer>
     )}
   </div>
 );
