8467dea0958168dac648283165e6aba61d287d40
raitalharehman <talharaay@gmail.com>
2019-03-08 11:46:38 +0500

    overlay__header-text class added and tag change in password overlay

--- src/components/OverlayManager/Password/index.tsx
+++ src/components/OverlayManager/Password/index.tsx
@@ -17,26 +17,26 @@ import closeImg from "../../../images/x.svg";
 const Password: React.FC<{ overlay: OverlayContextInterface }> = ({
   overlay
 }) => (
-  <Overlay context={overlay}>
-    <div className="password-reset">
-      <Online>
-        <div className="overlay__header">
-          <p>Reset your password</p>
-          <ReactSVG
-            path={closeImg}
-            onClick={overlay.hide}
-            className="overlay__header__close-icon"
-          />
-        </div>
-        <div className="password-reset__content">
-          <PasswordResetForm />
-        </div>
-      </Online>
-      <Offline>
-        <OfflinePlaceholder />
-      </Offline>
-    </div>
-  </Overlay>
-);
+    <Overlay context={overlay}>
+      <div className="password-reset">
+        <Online>
+          <div className="overlay__header">
+            <div className="overlay__header-text">Reset your password</div>
+            <ReactSVG
+              path={closeImg}
+              onClick={overlay.hide}
+              className="overlay__header__close-icon"
+            />
+          </div>
+          <div className="password-reset__content">
+            <PasswordResetForm />
+          </div>
+        </Online>
+        <Offline>
+          <OfflinePlaceholder />
+        </Offline>
+      </div>
+    </Overlay>
+  );
 
 export default Password;
