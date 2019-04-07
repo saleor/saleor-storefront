2b3e767aebe15689f13a5b215c09106d10e71f6c
raitalharehman <talharaay@gmail.com>
2019-04-07 19:01:25 +0500

    rename from  urlRules to slugUrl

--- src/components/App/routes.tsx
+++ src/components/App/routes.tsx
@@ -15,12 +15,12 @@ import OrderConfirmation from "../../views/OrderConfirmation/View";
 import { ProductPage } from "../../views/Product";
 import SearchPage from "../../views/Search";
 
-const urlRules = ':slug([a-z-0-9]+)/:id([0-9]+)/';
+const slugUrl = ':slug([a-z-0-9]+)/:id([0-9]+)/';
 export const baseUrl = "/";
 export const searchUrl = `${baseUrl}search/`;
-export const categoryUrl = `${baseUrl}category/${urlRules}`;
-export const collectionUrl = `${baseUrl}collection/${urlRules}`;
-export const productUrl = `${baseUrl}product/${urlRules}`;
+export const categoryUrl = `${baseUrl}category/${slugUrl}`;
+export const collectionUrl = `${baseUrl}collection/${slugUrl}`;
+export const productUrl = `${baseUrl}product/${slugUrl}`;
 export const cartUrl = `${baseUrl}cart/:token?/`;
 export const checkoutLoginUrl = `${baseUrl}login/`;
 export const pageUrl = `${baseUrl}page/:slug/`;
