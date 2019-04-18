b3c9dcd832c8c12a920af13a458dbaa020affdd9
AlicjaSzu <alicja.szukuc@gmail.com>
2019-04-18 17:51:30 +0200

    update in AddressPicker

--- src/checkout/components/AddressPicker/Picker.tsx
+++ src/checkout/components/AddressPicker/Picker.tsx
@@ -4,25 +4,32 @@ import classNames from "classnames";
 import React from "react";
 import ReactSVG from "react-svg";
 
-import { OverlayContext, OverlayTheme, OverlayType } from "../../../components";
+import {
+  Modal,
+  OverlayContext,
+  OverlayTheme,
+  OverlayType
+} from "../../../components";
 import AddressSummary from "../../../components/AddressSummary";
+import { AddNewShippingAddressForm } from "../../../components/ShippingAddressForm";
+import { FormAddressType } from "../../../components/ShippingAddressForm/types";
 import { Option } from "../../components";
-import { Address } from "../../types/Address";
-import AddNewAddressModal from "./Modal";
 
 import plusSvg from "../../../images/plus.svg";
 
-const Picker: React.FC<{
-  addresses: Address[];
-  selectedAddress?: Address;
-  onSelect(address: Address): void;
-  onAddNew?(address: Address, select: boolean): void;
+const Picker: React.SFC<{
+  addresses: FormAddressType[];
+  selectedAddress?: FormAddressType;
+  onSelect: (address: FormAddressType) => void;
+  onAddNew: (address: FormAddressType, select: boolean) => void;
 }> = ({ addresses, selectedAddress, onSelect, onAddNew }) => (
   <div className="address-picker">
     {addresses.map((address, id) => (
       <div
         key={id}
-        onClick={() => onSelect(address)}
+        onClick={() => {
+          onSelect(address);
+        }}
         className={classNames("address-picker__address", {
           "address-picker__address--selected": selectedAddress === address
         })}
@@ -44,7 +51,20 @@ const Picker: React.FC<{
             onClick={() => {
               overlay.show(OverlayType.modal, OverlayTheme.modal, {
                 content: (
-                  <AddNewAddressModal onAddNew={onAddNew} hide={overlay.hide} />
+                  <Modal
+                    title="Add New Address"
+                    loading={false}
+                    formId={"new-address-form"}
+                    hide={overlay.hide}
+                    submitBtnText={"Save"}
+                    cancelBtnText={"Cancel"}
+                  >
+                    <AddNewShippingAddressForm
+                      loading={false}
+                      errors={[]}
+                      onSubmit={onAddNew}
+                    />
+                  </Modal>
                 )
               });
             }}
