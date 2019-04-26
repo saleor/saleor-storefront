68548e0ff9556b572fff277dd0a6518b50647f04
AlicjaSzu <alicja.szukuc@gmail.com>
2019-04-26 14:48:49 +0200

    revert AddressPicker to statless component

--- src/checkout/components/AddressPicker/Picker.tsx
+++ src/checkout/components/AddressPicker/Picker.tsx
@@ -7,91 +7,78 @@ import ReactSVG from "react-svg";
 import { Modal } from "../../../components";
 import AddressSummary from "../../../components/AddressSummary";
 import { AddNewShippingAddressForm } from "../../../components/ShippingAddressForm";
-import { FormAddressType } from "../../../components/ShippingAddressForm/types";
 import { Option } from "../../components";
-import { IAddressPickerProps, IAddressPickerState } from "../../types";
+import { IAddressPickerProps } from "../../types";
 
 import plusSvg from "../../../images/plus.svg";
 
-class AddressPicker extends React.Component<
-  IAddressPickerProps,
-  IAddressPickerState
-> {
-  readonly state = {
-    showModal: false
-  };
-
-  onSubmitHandler = (data: FormAddressType) => {
-    this.props.onSubmit(data).then(response => {
-      const hasErrors = response.errors.length;
-      this.setState({ showModal: !!hasErrors });
-      if (!hasErrors) {
-        this.props.onAddNew(data);
-      }
-    });
-  };
-
-  renderAddressesList = () =>
-    this.props.addresses.map((address, id) => {
-      const isSelected = this.props.selectedAddress === address;
-      return (
-        <div
-          key={id}
-          onClick={() => {
-            this.props.onSelect(address);
-          }}
-          className={classNames("address-picker__address", {
-            "address-picker__address--selected": isSelected
-          })}
-        >
-          <AddressSummary address={address} />
-          <Option
-            selected={isSelected}
-            value={`${id}`}
-            label="Deliver to this address"
-          />
-        </div>
-      );
-    });
-
-  renderModalForm = () => (
-    <Modal
-      show={this.state.showModal}
-      title="Add New Address"
-      loading={this.props.loading}
-      formId="new-address-form"
-      hide={() => this.changeModalVisibility(false)}
-      submitBtnText="Add Address"
-      cancelBtnText="Cancel"
-    >
-      <AddNewShippingAddressForm
-        billing={this.props.billing}
-        loading={this.props.loading}
-        errors={this.props.errors}
-        onSubmit={this.onSubmitHandler}
-      />
-    </Modal>
-  );
-
-  changeModalVisibility = (showModal: boolean) => this.setState({ showModal });
-
-  render() {
+const renderAddressesList = ({
+  addresses,
+  onSelect,
+  selectedAddress
+}: IAddressPickerProps) =>
+  addresses.map((address, id) => {
+    const isSelected = selectedAddress === address;
     return (
-      <div className="address-picker">
-        {this.renderAddressesList()}
-        <div
-          className="address-picker__address address-picker__address--add-new"
-          onClick={() => this.changeModalVisibility(true)}
-        >
-          <div>
-            <ReactSVG path={plusSvg} />
-            <p>Add address</p>
-          </div>
-        </div>
-        {this.renderModalForm()}
+      <div
+        key={id}
+        onClick={() => {
+          onSelect(address);
+        }}
+        className={classNames("address-picker__address", {
+          "address-picker__address--selected": isSelected
+        })}
+      >
+        <AddressSummary address={address} />
+        <Option
+          selected={isSelected}
+          value={`${id}`}
+          label="Deliver to this address"
+        />
       </div>
     );
-  }
-}
+  });
+
+const renderModalForm = ({
+  billing,
+  errors,
+  handleAddressAdd,
+  hideAddNewModalForm,
+  isVisibleAddNewModalForm,
+  loading
+}: IAddressPickerProps) => (
+  <Modal
+    show={isVisibleAddNewModalForm}
+    title="Add New Address"
+    loading={loading}
+    formId="new-address-form"
+    hide={hideAddNewModalForm}
+    submitBtnText="Add Address"
+    cancelBtnText="Cancel"
+  >
+    <AddNewShippingAddressForm
+      billing={billing}
+      loading={loading}
+      errors={errors}
+      onSubmit={handleAddressAdd}
+    />
+  </Modal>
+);
+
+const AddressPicker: React.SFC<IAddressPickerProps> = props => (
+  <div className="address-picker">
+    {renderAddressesList(props)}
+    <div
+      className="address-picker__address address-picker__address--add-new"
+      onClick={props.showAddNewModalForm}
+    >
+      <div>
+        <ReactSVG path={plusSvg} />
+        <p>Add address</p>
+      </div>
+    </div>
+    {renderModalForm(props)}
+  </div>
+);
 
 export default AddressPicker;
