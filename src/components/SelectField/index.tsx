537c9aced8276898646d8f6021b4ad47a26ed11e
AlicjaSzu <alicja.szukuc@gmail.com>
2019-04-29 15:08:42 +0200

    add styleType prop to TextField and SelectField

--- src/components/SelectField/index.tsx
+++ src/components/SelectField/index.tsx
@@ -5,6 +5,8 @@ import Select from "react-select";
 // tslint:disable
 import { Props as SelectProps } from "react-select/lib/Select";
 
+type Style = "white" | "grey";
+
 export interface SelectValue {
   label: string;
   value: string;
@@ -12,6 +14,7 @@ export interface SelectValue {
 
 export interface SelectFieldProps<TValue> extends SelectProps<TValue> {
   label?: string;
+  styleType?: Style;
 }
 
 type GenericSelectField<TValue> = React.StatelessComponent<
@@ -20,9 +23,14 @@ type GenericSelectField<TValue> = React.StatelessComponent<
 
 const SelectField: GenericSelectField<SelectValue> = ({
   label = "",
+  styleType = "white",
   ...rest
 }) => (
-  <div className="react-select-wrapper">
+  <div
+    className={`react-select-wrapper ${
+      styleType === "grey" ? "react-select-wrapper--grey" : ""
+    }`}
+  >
     {label ? <span className="input__label">{label}</span> : null}
     <Select classNamePrefix="react-select" {...rest} />
   </div>
