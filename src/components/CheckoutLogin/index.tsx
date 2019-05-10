00b9d7b6349b070bced307436b6b9a497b3f138c
Mateusz Kula <me@mateuszkula.com>
2019-05-10 10:20:49 +0200

    Recreate CheckoutLogin as functional component

--- src/components/CheckoutLogin/index.tsx
+++ src/components/CheckoutLogin/index.tsx
@@ -1,6 +1,6 @@
 import "./scss/index.scss";
 
-import * as React from "react";
+import React, { useState } from "react";
 import { Redirect, RouteComponentProps } from "react-router";
 
 import { Offline, OfflinePlaceholder, Online, OverlayContext } from "..";
@@ -12,59 +12,53 @@ import CheckoutAsGuest from "./CheckoutAsGuest";
 import ResetPasswordForm from "./ResetPasswordForm";
 import SignInForm from "./SignInForm";
 
-class CheckoutLogin extends React.PureComponent<
-  RouteComponentProps<{}>,
-  { resetPassword: boolean }
-> {
-  state = { resetPassword: false };
-
-  render() {
-    return (
-      <OverlayContext.Consumer>
-        {overlayContext => {
-          return (
-            <UserContext.Consumer>
-              {({ user }) => {
-                if (user) {
-                  return <Redirect to={checkoutUrl} />;
-                }
-                return (
-                  <div className="container">
-                    <Online>
-                      <div className="checkout-login">
-                        <CheckoutAsGuest
-                          overlayContext={overlayContext}
-                          checkoutUrl={checkoutUrl}
-                        />
-                        <div className="checkout-login__user">
-                          {this.state.resetPassword ? (
-                            <ResetPasswordForm
-                              onClick={() => {
-                                this.setState({ resetPassword: false });
-                              }}
-                            />
-                          ) : (
-                            <SignInForm
-                              onClick={() => {
-                                this.setState({ resetPassword: true });
-                              }}
-                            />
-                          )}
-                        </div>
+const CheckoutLogin: React.FC<{}> = () => {
+  const [resetPassword, setResetPassword] = useState(false);
+  return (
+    <OverlayContext.Consumer>
+      {overlayContext => {
+        return (
+          <UserContext.Consumer>
+            {({ user }) => {
+              if (user) {
+                return <Redirect to={checkoutUrl} />;
+              }
+              return (
+                <div className="container">
+                  <Online>
+                    <div className="checkout-login">
+                      <CheckoutAsGuest
+                        overlayContext={overlayContext}
+                        checkoutUrl={checkoutUrl}
+                      />
+                      <div className="checkout-login__user">
+                        {resetPassword ? (
+                          <ResetPasswordForm
+                            onClick={() => {
+                              setResetPassword(false);
+                            }}
+                          />
+                        ) : (
+                          <SignInForm
+                            onClick={() => {
+                              setResetPassword(true);
+                            }}
+                          />
+                        )}
                       </div>
-                    </Online>
-                    <Offline>
-                      <OfflinePlaceholder />
-                    </Offline>
-                  </div>
-                );
-              }}
-            </UserContext.Consumer>
-          );
-        }}
-      </OverlayContext.Consumer>
-    );
-  }
-}
+                    </div>
+                  </Online>
+                  <Offline>
+                    <OfflinePlaceholder />
+                  </Offline>
+                </div>
+              );
+            }}
+          </UserContext.Consumer>
+        );
+      }}
+    </OverlayContext.Consumer>
+  );
+};
 
 export default CheckoutLogin;
