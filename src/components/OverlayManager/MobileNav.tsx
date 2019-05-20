2e2e08a1a9896cf779e392af9a869bee8cc84ec1
Mateusz Kula <me@mateuszkula.com>
2019-05-20 13:48:03 +0200

    Add support for left swine in mobile nav

--- src/components/OverlayManager/MobileNav.tsx
+++ src/components/OverlayManager/MobileNav.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
 
 import { INavItem, MobileNavList, Overlay, OverlayContextInterface } from "..";
+import { useSwipeable } from "react-swipeable";
 
 const MobileNav: React.FC<{ overlay: OverlayContextInterface }> = ({
   overlay
 }) => {
   const items: INavItem[] = overlay.context.data;
+  const config = {
+    delta: 50
+  };
+  const handlers = useSwipeable({
+    onSwipedLeft: eventData => overlay.hide(),
+    ...config
+  });
 
   return (
-    <Overlay context={overlay}>
-      <div className="side-nav" onClick={evt => evt.stopPropagation()}>
-        <MobileNavList items={items} hideOverlay={overlay.hide} />
-      </div>
-    </Overlay>
+    <div {...handlers}>
+      <Overlay context={overlay}>
+        <div className="side-nav" onClick={evt => evt.stopPropagation()}>
+          <MobileNavList items={items} hideOverlay={overlay.hide} />
+        </div>
+      </Overlay>
+    </div>
   );
 };
 
