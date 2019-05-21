80c8cb3957c2ce9666a7c8461f700aba95e95eeb
Mateusz Kula <me@mateuszkula.com>
2019-05-21 10:49:21 +0200

    Add redesigned version of 404 page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,6 +1,9 @@
 import "./scss/index.scss";
 
 import * as React from "react";
+import { Link } from "react-router-dom";
+import { BASE_URL } from "../../core/config";
+import Button from "../Button";
 
 interface NotFoundProps {
   message?: string;
@@ -8,9 +11,18 @@ interface NotFoundProps {
 
 const NotFound: React.FC<NotFoundProps> = ({ message }) => (
   <div className="not-found-page">
-    <h2 className="not-found-page__header">
-      {message || "Sorry, we couldn't find the page you're looking for :("}
-    </h2>
+    <h2 className="not-found-page__header">404</h2>
+    <div className="not-found-page__ruler" />
+    <div className="not-found-page__message">
+      <p>We can’t seem to find a page you are looking for! </p>
+      <p>You may have mistyped the address or the page may have moved. </p>
+      <p>We’re sorry for the error and hope you’ll have a good day.</p>
+    </div>
+    <div className="not-found-page__button">
+      <Link to={BASE_URL}>
+        <Button secondary>Back to home</Button>
+      </Link>
+    </div>
   </div>
 );
 
