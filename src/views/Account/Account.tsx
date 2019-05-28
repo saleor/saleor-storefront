427f1f252cfb5eb6da5bc99ef6a186862402fce0
Mateusz Kula <me@mateuszkula.com>
2019-05-28 11:36:04 +0200

    Add spinner for screen loading

--- src/views/Account/Account.tsx
+++ src/views/Account/Account.tsx
@@ -1,23 +1,28 @@
-import "./scss/index.scss";
-
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import AccountNavigation from "../../account/AccountNavigation";
 import HelloPrompt from "../../account/HelloPrompts";
-import { NotFound } from "../../components";
+import { Loader, NotFound } from "../../components";
 import { UserContext } from "../../components/User/context";
 
 const Account: React.FC<RouteComponentProps> = ({ match }) => {
-  const user = React.useContext(UserContext);
+  const { user, loading } = React.useContext(UserContext);
   const links = ["account", "order-history", "address-book", "payment-options"];
-  return user.user ? (
-    <div className="account-container">
-      <HelloPrompt name={user.user.firstName} />
-      <AccountNavigation links={links} active={match.path.replace(/\//g, "")} />
-    </div>
-  ) : (
-    <NotFound />
-  );
+  if (loading) {
+    return <Loader />;
+  }
+  if (user) {
+    return (
+      <div className="container">
+        <HelloPrompt name={user.firstName} />
+        <AccountNavigation
+          links={links}
+          active={match.path.replace(/\//g, "")}
+        />
+      </div>
+    );
+  }
+  return <NotFound />;
 };
 
 export default Account;
