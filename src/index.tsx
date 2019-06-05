c0bd8ff518601f8e2b5d4b07aa987687e73df727
Adam Bogdał <adam@bogdal.pl>
2019-06-05 14:19:52 +0200

    Refresh the page when new app is ready

--- src/index.tsx
+++ src/index.tsx
@@ -150,7 +150,18 @@ const startApp = async () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed' &&
+                navigator.serviceWorker.controller) {
+              location.reload();
+            }
+          }
+        }
+      }); 
   });
 }
 
