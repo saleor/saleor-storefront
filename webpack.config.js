81f3a5c952df4b9713b543b5565e51b182436ca5
jwm0 <majorek.jakub@gmail.com>
2019-06-07 18:00:36 +0200

    Split webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,148 +1,23 @@
 const path = require("path");
+const merge = require('webpack-merge');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebappWebpackPlugin = require("webapp-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const webpack = require("webpack");
+const baseConfig = require("./config/webpack/config.base");
+const devConfig = require("./config/webpack/config.dev");
+const prodConfig = require("./config/webpack/config.prod");
+const workerConfig = require("./config/webpack/config.worker");
 
 const sourceDir = path.join(__dirname, "./src");
 const distDir = path.join(__dirname, "./dist");
 
-
 module.exports = (env, argv) => {
   const devMode = argv.mode !== "production";
-  return {
-    resolve: {
-      alias: {
-        "react-dom": "@hot-loader/react-dom"
-      },
-      extensions: [".ts", ".tsx", ".js", ".jsx"],
-      plugins: [
-        new TsconfigPathsPlugin({
-          configFile: "./tsconfig.json"
-        })
-      ]
-    },
-    entry: {
-      app: `${sourceDir}/index.tsx`
-    },
-    output: {
-      path: distDir,
-      filename: devMode ? "js/[name].js" : "js/[name].[contenthash].js",
-      publicPath: "/"
-    },
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: "ts-loader",
-          exclude: /node_modules/,
-          options: {
-            experimentalWatchApi: true,
-            transpileOnly: true
-          }
-        },
-        {
-          test: /\.(scss|css)$/,
-          use: [
-            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-            {
-              loader: "css-loader",
-              options: { sourceMap: true }
-            },
-            { loader: "sass-loader" }
-          ]
-        },
-        {
-          test: /\.(woff2?|ttf|eot)$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "fonts/",
-                publicPath: "/fonts/"
-              }
-            }
-          ]
-        },
-        {
-          test: /\.(gif|jpg|png|svg)$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "images/",
-                publicPath: "/images/"
-              }
-            },
-            {
-              loader: "image-webpack-loader",
-              options: {
-                mozjpeg: {
-                  progressive: true,
-                  quality: 85
-                },
-                pngquant: {
-                  quality: "65-90",
-                  speed: 4
-                },
-                gifsicle: {
-                  enabled: false
-                }
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin([distDir]),
-      new HtmlWebpackPlugin({
-        filename: `${distDir}/index.html`,
-        template: `${sourceDir}/index.html`
-      }),
-      new MiniCssExtractPlugin({
-        filename: devMode ? "[name].css" : "[name].[hash].css",
-        chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
-      }),
-      new ForkTsCheckerWebpackPlugin({
-        tslint: true,
-        exclude: "node_modules",
-        async: false
-      }),
-      // DEV mode plugins
-      ...(devMode ? [
-        new webpack.HotModuleReplacementPlugin()
-      ] : []),
-      // PWA plugins
-      new WebappWebpackPlugin({
-        logo: `${sourceDir}/images/favicon.png`,
-        prefix: "images/favicons/",
-        favicons: {
-          appName: "Saleor",
-          appDescription: "Storefront for the Saleor e-commerce platform",
-          display: "standalone",
-          developerURL: null, // prevent retrieving from the nearest package.json
-          background: "#ddd",
-          theme_color: "#333"
-        }
-      }),
-      new WorkboxPlugin.InjectManifest({
-        swSrc: `${sourceDir}/sw.js`,
-        swDest: path.join(distDir, './service-worker.js'),
-        exclude: [/\.map$/, /^manifest.*\.js(?:on)?$/, /\.js.map$/, /\.css.map/, /\.xls$/]
-      }),
-      new webpack.EnvironmentPlugin(["npm_package_version", "BACKEND_URL"])
-    ],
-    node: {
-      fs: "empty"
-    }
-  };
-};
+  const sw = !!argv['service-worker'];
+  const paths = { sourceDir, distDir };
+
+  const base = baseConfig(paths);
+  const worker = workerConfig(paths);
+  const dev = sw ? merge(base, worker, devConfig(paths)) : merge(base, devConfig(paths));
+  const prod = merge(base, worker, prodConfig(paths));
+
+  return devMode ? dev : prod;
+}
