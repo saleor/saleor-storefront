cf527dc92a0336686ed660e7585c2d488558c24a
Mateusz Kula <me@mateuszkula.com>
2019-06-13 10:54:23 +0200

    Add CreditCardIcon component

---
+++ src/@next/components/atoms/CreditCardIcon/CreditCardIcon.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import ReactSVG from "react-svg";
+
+import amexImg from "images/amex.svg";
+import discoverImg from "images/discover.svg";
+import jcbImg from "images/jcb.svg";
+import maestroImg from "images/maestro.svg";
+import mastercardImg from "images/mastercard.svg";
+import visaImg from "images/visa.svg";
+
+import { IProps } from "./types";
+
+const providers = new Map();
+
+providers.set("visa", visaImg);
+providers.set("maestro", maestroImg);
+providers.set("mastercard", mastercardImg);
+providers.set("amex", amexImg);
+providers.set("jcb", jcbImg);
+providers.set("discover", discoverImg);
+
+export const CreditCardIcon: React.FC<IProps> = ({ provider }) => {
+  return (
+    <>
+      {providers.has(provider) ? (
+        <ReactSVG path={providers.get(provider)} />
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
