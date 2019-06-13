f2f0c01a0eea6d0d570a067992a5c48f27196bc9
jwm0 <majorek.jakub@gmail.com>
2019-06-13 12:53:18 +0200

    Add structural tests and migrate to babel v7

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,4 +2,4 @@ import "@storybook/addon-options/register";
 import "@storybook/addon-knobs/register";
 import "@storybook/addon-storysource/register";
 import "@storybook/addon-viewport/register";
-import "storybook-addon-styled-component-theme/dist/src/register";
+import "storybook-styled-components/register";
