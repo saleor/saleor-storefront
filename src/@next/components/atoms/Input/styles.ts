a9d40616b808cfab268f82bbf099525b60308c14
jwm0 <majorek.jakub@gmail.com>
2019-06-19 17:59:50 +0200

    Make input label bg respond to parent bg color

--- src/@next/components/atoms/Input/styles.ts
+++ src/@next/components/atoms/Input/styles.ts
@@ -63,12 +63,11 @@ export const Input = styled.input`
   background-color: transparent;
 `;
 
-export const Label = styled.label<{ active: boolean }>`
+export const Label = styled.label<{ active: boolean; bgColor: string | null }>`
   position: absolute;
   left: ${props => (props.active ? "0.5rem" : "1rem")};
   padding: 0 ${props => (props.active ? 0.5 : 0)}rem;
-  background-color: ${props =>
-    props.active ? props.theme.colors.white : "transparent"};
+  background-color: ${props => (props.active ? props.bgColor : "transparent")};
   font-size: ${props =>
     props.active
       ? props.theme.input.labelFontSize
