b653884641c248353a679c1d9211d5afe84266fd
jwm0 <majorek.jakub@gmail.com>
2019-06-26 13:05:46 +0200

    Include jest-styled-components serializer

--- .storybook/__tests__/storyshots.test.js
+++ .storybook/__tests__/storyshots.test.js
@@ -1,6 +1,9 @@
-import "jest-styled-components";
 import initStoryshots from "@storybook/addon-storyshots";
 import { multiSnapshotWithOptions } from "@storybook/addon-storyshots/dist/test-bodies";
+import styleSheetSerializer from "jest-styled-components/src/styleSheetSerializer";
+import { addSerializer } from "jest-specific-snapshot";
+
+addSerializer(styleSheetSerializer);
 
 initStoryshots({
   framework: "react",
