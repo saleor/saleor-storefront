c6c8a975249974f9d39f632239f6cb43c62f5f89
AlicjaSzu <alicja.szukuc@gmail.com>
2019-06-26 20:53:56 +0200

    fixes after rebase

--- .storybook/__tests__/storyshots.test.js
+++ .storybook/__tests__/storyshots.test.js
@@ -1,12 +1,9 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { multiSnapshotWithOptions } from "@storybook/addon-storyshots/dist/test-bodies";
-<<<<<<< HEAD
 import styleSheetSerializer from "jest-styled-components/src/styleSheetSerializer";
 import { addSerializer } from "jest-specific-snapshot";
 
 addSerializer(styleSheetSerializer);
-=======
->>>>>>> Updated storyshots config
 
 initStoryshots({
   framework: "react",
