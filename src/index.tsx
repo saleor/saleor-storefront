e7981b3ced47c35d5df9222743dc103d1e02bc51
AlicjaSzu <alicja.szukuc@gmail.com>
2019-06-27 18:22:18 +0200

    check for circle

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,7 @@ import CheckoutProvider from "./checkout/provider";
 import { baseUrl as checkoutBaseUrl } from "./checkout/routes";
 import { apiUrl, serviceWorkerTimeout } from "./constants";
 import { history } from "./history";
-import { loadCatalogs } from "./translations";
+// import { loadCatalogs } from "./translations";
 
 import { OverlayProvider, UserProvider } from "./components";
 
@@ -78,7 +78,25 @@ const startApp = async () => {
     timeout: 2500,
   };
 
-  const catalogs = await loadCatalogs();
+  // const catalogs = {} // await loadCatalogs();
+
+  const fallbackLang = "en";
+
+  const getLangCode = (code: string) =>
+    code.includes("-") ? code.split("-")[0] : code;
+  const lang = getLangCode(
+    (
+      (navigator.languages && navigator.languages[0]) ||
+      navigator.language ||
+      fallbackLang
+    ).toLowerCase()
+  );
+  // tslint:disable-next-line:no-console
+  console.log("lang", lang);
+  export const loadCatalogs = (language: string = lang) =>
+    import(`@lingui/loader!./locales/${language}/messages.po`);
+  
+  const catalogs = loadCatalogs(lang) || {};
 
   const Root = hot(module)(() => {
     const alert = useAlert();
@@ -108,7 +126,7 @@ const startApp = async () => {
     return (
       <Router history={history}>
         <ApolloProvider client={apolloClient}>
-          <I18nProvider catalogs={catalogs}>
+          <I18nProvider language={lang} catalogs={catalogs}>
             <ShopProvider>
               <OverlayProvider>
                 <UserProviderWithTokenHandler
