51efaa610613c4079441a5b83d3414b84245bd3d
AlicjaSzu <alicja.szukuc@gmail.com>
2019-06-27 12:00:42 +0200

    add getLangCode check

--- src/translations/loadMessagesCatalog.tsx
+++ src/translations/loadMessagesCatalog.tsx
@@ -1,9 +1,14 @@
 const fallbackLang = "en";
-const lang = (
-  (navigator.languages && navigator.languages[0]) ||
-  navigator.language ||
-  fallbackLang
-).toLowerCase();
+
+const getLangCode = (code: string) =>
+  code.includes("-") ? code.split("-")[0] : code;
+const lang = getLangCode(
+  (
+    (navigator.languages && navigator.languages[0]) ||
+    navigator.language ||
+    fallbackLang
+  ).toLowerCase()
+);
 
 export const loadCatalogs = (language: string = lang) =>
   import(`@lingui/loader!../locales/${language}/messages.po`);
