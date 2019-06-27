b6dedb9aa2acba644de27abb06d7d453ec0023d9
Mateusz Kula <me@mateuszkula.com>
2019-06-27 15:45:18 +0200

    Update test description

--- src/@next/components/atoms/AddNewTile/test.tsx
+++ src/@next/components/atoms/AddNewTile/test.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { AddNewTile } from ".";
 
 describe("<AddNewTile />", () => {
-  it("should display child component with text from passed props", () => {
+  it("should display child component with card type passed as prop and rendered inside it as a text", () => {
     const wrapper = mount(<AddNewTile type="card" />);
 
     expect(wrapper.exists()).toEqual(true);
