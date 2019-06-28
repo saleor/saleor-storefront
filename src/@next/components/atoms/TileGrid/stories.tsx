91b31576e5e0e4244a93471a8b2f9058801ec3e2
Mateusz Kula <me@mateuszkula.com>
2019-06-28 12:29:56 +0200

    Fix TileGrid storie file

--- src/@next/components/atoms/TileGrid/stories.tsx
+++ src/@next/components/atoms/TileGrid/stories.tsx
@@ -1,9 +1,8 @@
+import { storiesOf } from "@storybook/react";
 import React from "react";
 
-import styled from "styled-components";
 import { TileGrid } from ".";
 import { Tile } from "../";
-import { createStory } from "../baseStory";
 
 const tile = (
   <Tile header={<>This is header</>} footer={<>This is footer</>}>
@@ -12,6 +11,6 @@ const tile = (
 );
 
 const tiles = [tile, tile, tile, tile, tile, tile];
-createStory("TileGrid")
+storiesOf("@components/atoms/TileGrid", module)
   .add("default", () => <TileGrid elements={tiles} />)
   .add("custom grid size", () => <TileGrid elements={tiles} md={6} lg={6} />);
