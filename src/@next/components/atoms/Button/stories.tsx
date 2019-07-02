30da80960d230820d625be70a44ad0d23eb3261d
AlicjaSzu <alicja.szukuc@gmail.com>
2019-07-02 17:23:25 +0200

    add size prop to Button atom

--- src/@next/components/atoms/Button/stories.tsx
+++ src/@next/components/atoms/Button/stories.tsx
@@ -5,4 +5,5 @@ import { Button } from ".";
 
 storiesOf("@components/atoms/Button", module)
   .add("Primary", () => <Button>Primary Button</Button>)
-  .add("Secondary", () => <Button color="secondary">Secondary Button</Button>);
+  .add("Secondary", () => <Button color="secondary">Secondary Button</Button>)
+  .add("Size sm", () => <Button size="sm">Small Button</Button>);
