331e8d2b1cc9227514c3b39c952e444c5fd6b74e
AlicjaSzu <alicja.szukuc@gmail.com>
2019-07-02 17:44:05 +0200

    start FormFooter tests

--- src/@next/components/molecules/FormFooter/test.tsx
+++ src/@next/components/molecules/FormFooter/test.tsx
@@ -2,13 +2,46 @@ import { shallow } from "enzyme";
 import "jest-styled-components";
 import React from "react";
 
+import { Button, ButtonLink } from "@components/atoms";
 import { FormFooter } from ".";
+import { IProps } from "./types";
 
 describe("<FormFooter />", () => {
-  // Example test
-  it("exists", () => {
-    const wrapper = shallow(<FormFooter />);
+  const submitBtnText = "Save";
+  const onSubmit = jest.fn();
 
-    expect(wrapper.exists()).toEqual(true);
+  const DEFAULT_PROPS = {
+    submitBtn: {
+      action: onSubmit,
+      text: submitBtnText,
+    },
+  };
+
+  const renderFormFooter = (props: IProps) =>
+    shallow(<FormFooter {...props} />);
+
+  it("exists", () => {
+    const formFooter = renderFormFooter(DEFAULT_PROPS);
+
+    expect(formFooter.exists()).toEqual(true);
+  });
+
+  it("should render < Button />", () => {
+    const button = renderFormFooter(DEFAULT_PROPS).find(Button);
+
+    expect(button.exists()).toEqual(true);
+    expect(button.prop("onClick")).toEqual(onSubmit);
+  });
+
+  it("should render <ButtonLink />", () => {
+    const onCancel = jest.fn();
+    const cancelBtnText = "Cancel";
+    const button = renderFormFooter({
+      ...DEFAULT_PROPS,
+      cancelBtn: { action: onCancel, text: cancelBtnText },
+    }).find(ButtonLink);
+
+    expect(button.exists()).toEqual(true);
+    expect(button.prop("onClick")).toEqual(onCancel);
   });
 });
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import ReactSVG from "react-svg";
 
-import { Button } from "..";
+import { FormFooter } from "@components/molecules";
 import closeImg from "../../images/modal-close.svg";
 
 interface IModalProps {
@@ -44,23 +44,11 @@ const Modal: React.FC<IModalProps> = ({
                 />
               </div>
               <div className="modal__content">{children}</div>
-              <div className="modal__footer">
-                {cancelBtnText && (
-                  <button className="modal__cancelBtn" onClick={hide}>
-                    {cancelBtnText}
-                  </button>
-                )}
-                {submitBtnText && (
-                  <Button
-                    type="submit"
-                    form={formId}
-                    disabled={loading}
-                    className="modal__button"
-                  >
-                    {loading ? "Loading" : submitBtnText}
-                  </Button>
-                )}
-              </div>
+              <FormFooter
+                cancelBtn={{ action: hide, text: cancelBtnText }}
+                submitBtn={{ text: loading ? "Loading" : submitBtnText }}
+                formId={formId}
+              />
             </div>
           </div>
         </div>,
