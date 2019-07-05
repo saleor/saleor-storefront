a4a998951c74c268f0acb449c6d77ce22a1d0aa8
Mateusz Kula <me@mateuszkula.com>
2019-07-05 10:43:44 +0200

    Remove flexboxgrid leftover config

--- src/@next/components/atoms/TileGrid/styles.tsx
+++ src/@next/components/atoms/TileGrid/styles.tsx
@@ -9,15 +9,15 @@ export const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin: 0;
-  margin-top: ${props => `-${props.theme.flexboxgrid.gutterWidth}rem`};
-  margin-left: ${props => `-${props.theme.flexboxgrid.gutterWidth}rem`};
+  margin-top: ${props => `-${props.theme.spacing.gutter}`};
+  margin-left: ${props => `-${props.theme.spacing.gutter}`};
 `;
 
 export const Tile = styled.div<TileProps>`
   margin: 0;
   padding: 0;
-  padding-top: ${props => `${props.theme.flexboxgrid.gutterWidth}rem`};
-  padding-left: ${props => `${props.theme.flexboxgrid.gutterWidth}rem`};
+  padding-top: ${props => props.theme.spacing.gutter};
+  padding-left: ${props => props.theme.spacing.gutter};
   width: calc(100% / ${props => props.columns});
 
   ${media.smallScreen`
