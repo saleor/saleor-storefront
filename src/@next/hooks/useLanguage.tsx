4a22b7010455f14cd06db1c0f9fecb0b85d8e987
Adam Bogdał <adam@bogdal.pl>
2019-07-08 15:54:33 +0200

    Check existence of language

--- src/@next/hooks/useLanguage.tsx
+++ src/@next/hooks/useLanguage.tsx
@@ -2,6 +2,14 @@ import { I18nContext } from '@components/containers';
 import React from "react";
 
 export const useLanguage = () => {
-  const { language, setLanguage } = React.useContext(I18nContext);
-  return { language, setLanguage };
+  const { language, languages, setLanguage } = React.useContext(I18nContext);
+
+  const handleSetLanguage = (language:string) => {
+    if (!languages.hasOwnProperty(language)) {
+      throw new Error(`Language "${language}" is not supported`);
+    }
+    setLanguage(language);
+  }
+
+  return { language, setLanguage: handleSetLanguage };
 };
