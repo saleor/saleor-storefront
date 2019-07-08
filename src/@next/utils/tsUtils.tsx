75946e49850341443adecd068984bfa094a0771b
Mateusz Kula <me@mateuszkula.com>
2019-07-08 15:08:38 +0200

    Add DropdownMenu component

--- src/@next/utils/tsUtils.tsx
+++ src/@next/utils/tsUtils.tsx
@@ -14,3 +14,12 @@ export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
   }[Keys];
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export const maybe = <T,>(exp: () => T, d?: T) => {
+  try {
+    const result = exp();
+    return result === undefined ? d : result;
+  } catch {
+    return d;
+  }
+};
