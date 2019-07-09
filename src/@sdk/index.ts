647739a5278b94ba1c6748c654315531ced60009
jwm0 <majorek.jakub@gmail.com>
2019-07-09 15:45:06 +0200

    Introduce type definitions for queries

--- src/@sdk/index.ts
+++ src/@sdk/index.ts
@@ -1,5 +1,5 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloClient, ApolloError, ApolloQueryResult } from "apollo-client";
+import { ApolloClient, ApolloError } from "apollo-client";
 import { ApolloLink } from "apollo-link";
 import { BatchHttpLink } from "apollo-link-batch-http";
 import { RetryLink } from "apollo-link-retry";
@@ -8,18 +8,8 @@ import urljoin from "url-join";
 import { authLink, getAuthToken, invalidTokenLink, setAuthToken } from "./auth";
 import { MUTATIONS } from "./mutations";
 import { QUERIES } from "./queries";
-import { KeysMatching } from "./tsHelpers";
+import { InferOptions, ReturnData } from "./tsHelpers";
 import { getErrorsFromData } from "./utils";
-
-type InferOptions<T> = T extends (c, o: infer O) => any ? O : never;
-type NestedData<T> = KeysMatching<T, { [key: string]: any }>;
-type ReturnData<
-  T extends { [key: string]: (...args: any) => any },
-  N extends keyof T
-> = ReturnType<T[N]> extends Promise<infer R> ? NestedData<R["data"]> : never;
-
-type MutationData<T extends keyof MUTATIONS> = ReturnData<MUTATIONS, T>;
-type QueryData<T extends keyof QUERIES> = ReturnData<QUERIES, T>;
 
 const { invalidLink } = invalidTokenLink();
 const getLink = url =>
@@ -38,11 +28,44 @@ export const createSaleorClient = (url?: string, cache = new InMemoryCache()) =>
 
 export class SaleorAPI {
   client: ApolloClient<any>;
-  userId: string | null;
+
+  getProductDetails = this.fireQuery<QUERIES, "ProductDetails">(
+    QUERIES.ProductDetails
+  );
+
+  getUserOrderDetails = this.fireQuery<QUERIES, "UserOrders">(
+    QUERIES.UserOrders
+  );
 
   constructor(client: ApolloClient<any>) {
     this.client = client;
   }
+
+  signIn = (
+    variables: InferOptions<MUTATIONS["TokenAuth"]>["variables"],
+    options?: Omit<InferOptions<MUTATIONS["TokenAuth"]>, "variables">
+  ): Promise<ReturnData<MUTATIONS, "TokenAuth">> =>
+    new Promise(async (resolve, reject) => {
+      try {
+        const data = await this.fireQuery<MUTATIONS, "TokenAuth">(
+          MUTATIONS.TokenAuth
+        )(variables, {
+          ...options,
+          update: (proxy, data) => {
+            if (data.data.tokenCreate.token) {
+              setAuthToken(data.data.tokenCreate.token);
+            }
+            if (options && options.update) {
+              options.update(proxy, data);
+            }
+          },
+        });
+
+        resolve(data);
+      } catch (e) {
+        reject(e);
+      }
+    });
 
   attachAuthListener = (callback: (authenticated: boolean) => void) => {
     const eventHandler = () => {
@@ -60,84 +83,47 @@ export class SaleorAPI {
     return !!getAuthToken();
   };
 
-  // AUTH QUERY
-  setAuthToken = (
-    variables,
-    options: InferOptions<MUTATIONS["TokenAuth"]>
-  ): Promise<MutationData<"TokenAuth">> =>
-    new Promise(async (resolve, reject) => {
-      try {
-        const { data } = await MUTATIONS.TokenAuth(this.client, {
-          ...options,
-          variables,
-        });
-        const errors = getErrorsFromData(data);
-
-        // throw user input errors
-        if (errors) {
-          reject(new ApolloError({ extraInfo: errors }));
-        }
-
-        setAuthToken(data.tokenCreate.token);
-        this.userId = data.tokenCreate.user.id;
-
-        resolve(data.tokenCreate);
-      } catch (err) {
-        reject(err);
-      }
-    });
-
-  // TODO: Use query/mutation key once, infer throughout
-  getUserOrderDetails = (
-    variables,
-    options: InferOptions<QUERIES["UserOrders"]>
-  ) =>
-    this.fireQuery<QueryData<"UserOrders">>(
-      QUERIES.UserOrders,
-      variables,
-      options
-    );
-
-  getProductDetails = (variables, options = {}) =>
-    this.fireQuery<QueryData<"ProductDetails">>(
-      QUERIES.ProductDetails,
-      variables,
-      options
-    );
-
   // Query and mutation wrapper to catch errors
-  private fireQuery = <T>(query: any, variables, options): Promise<T> =>
-    new Promise(async (resolve, reject) => {
-      try {
-        const { data, errors: apolloErrors } = await query(this.client, {
-          ...options,
-          variables,
-        });
+  private fireQuery<
+    T extends { [key: string]: (...args: any) => any },
+    N extends keyof T
+  >(query: T[N]) {
+    return (
+      variables: InferOptions<T[N]>["variables"],
+      options?: Omit<InferOptions<T[N]>, "variables">
+    ): Promise<ReturnData<T, N>> =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const { data, errors: apolloErrors } = await query(this.client, {
+            ...options,
+            variables,
+          });
 
-        const userInputErrors = getErrorsFromData(data);
-        const errors =
-          apolloErrors ||
-          (userInputErrors
-            ? new ApolloError({ extraInfo: userInputErrors })
-            : null);
+          const userInputErrors = getErrorsFromData(data);
+          const errors =
+            apolloErrors ||
+            (userInputErrors
+              ? new ApolloError({ extraInfo: userInputErrors })
+              : null);
 
-        if (errors) {
-          reject(errors);
+          if (errors) {
+            reject(errors);
+          }
+
+          // IMPORTANT: this function relies on unique nested key names
+          const nestedData = Object.keys(data).reduce(
+            (acc, key) => ({
+              ...acc,
+              ...data[key],
+            }),
+            {}
+          );
+          const result = !!Object.keys(nestedData).length ? nestedData : null;
+
+          resolve(result);
+        } catch (error) {
+          reject(error);
         }
-
-        // IMPORTANT: this function relies on unique nested key names
-        const nestedData = Object.keys(data).reduce(
-          (acc, key) => ({
-            ...acc,
-            ...data[key],
-          }),
-          {}
-        );
-        const result = !!Object.keys(nestedData).length ? nestedData : null;
-
-        resolve(result);
-      } catch (error) {
-        reject(error);
-      }
-    });
+      });
+  }
 }
--- src/@sdk/tsHelpers.ts
+++ src/@sdk/tsHelpers.ts
@@ -1,3 +1,4 @@
+// general
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
@@ -12,3 +13,17 @@ export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
 export type KeysMatching<T, V> = T[{
   [K in keyof T]: T[K] extends V ? K : never;
 }[keyof T]];
+
+// sdk specific
+export type InferOptions<T> = T extends (c, o: infer O) => any ? O : never;
+
+type NestedData<T> = KeysMatching<T, { [key: string]: any }>;
+
+export type ReturnData<
+  T extends { [key: string]: (...args: any) => any },
+  N extends keyof T
+> = ReturnType<T[N]> extends Promise<infer R>
+  ? R extends { [key: string]: any }
+    ? NestedData<R["data"]>
+    : {}
+  : never;
