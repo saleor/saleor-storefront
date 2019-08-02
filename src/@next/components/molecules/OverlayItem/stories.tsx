f1951b18c671150d7b36e81a518bc54ef380826d
Adam Bogdał <adam@bogdal.pl>
2019-08-02 11:11:52 +0200

    Simplify the code

--- src/@next/components/molecules/OverlayItem/stories.tsx
+++ src/@next/components/molecules/OverlayItem/stories.tsx
@@ -1,23 +1,8 @@
-import { action } from "@storybook/addon-actions";
-import { boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
 import { OverlayItem } from ".";
 
 storiesOf("@components/molecules/OverlayItem", module)
-  .add("default", () => (
-    <OverlayItem
-      {...(boolean("Clickable", false) && { onClick: action("onClick") })}
-    >
-      Item
-    </OverlayItem>
-  ))
-  .add("selected", () => (
-    <OverlayItem
-      selected
-      {...(boolean("Clickable", false) && { onClick: action("onClick") })}
-    >
-      Item
-    </OverlayItem>
-  ));
+  .add("default", () => <OverlayItem>Item</OverlayItem>)
+  .add("selected", () => <OverlayItem selected>Item</OverlayItem>);
