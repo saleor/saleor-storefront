2f702bb17a9d3a5db8f543693589ff8fb44970a2
Adam Bogdał <adam@bogdal.pl>
2019-08-26 07:19:22 +0200

    Show loader

--- src/userAccount/views/OrderDetails/View.tsx
+++ src/userAccount/views/OrderDetails/View.tsx
@@ -3,6 +3,7 @@ import "./scss/index.scss";
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 
+import { Loader } from "@components/atoms";
 import { useOrderDetails, useUserDetails } from "@sdk/react";
 
 import Page from "./Page";
@@ -12,9 +13,13 @@ const View: React.FC<RouteComponentProps<{ token?: string }>> = ({
     params: { token },
   },
 }) => {
-  const { data: order } = useOrderDetails({ token });
+  const { data: order, loading } = useOrderDetails({ token });
   const { data: user } = useUserDetails();
   const guest = !user;
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <div className="order-details container">
