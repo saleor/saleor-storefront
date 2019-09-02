1f621de5b8829a86caf530b28b22a80e1b7575b0
Mateusz Kula <me@mateuszkula.com>
2019-09-02 10:02:31 +0200

    Remove rebase leftover code

--- src/@sdk/react/mutations.ts
+++ src/@sdk/react/mutations.ts
@@ -16,4 +16,3 @@ export const useUpdateCheckoutBillingAddress = mutationFactory(
 export const useUpdateCheckoutShippingAddress = mutationFactory(
   "setCheckoutShippingAddress"
 );
->>>>>>> master
