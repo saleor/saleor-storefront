3ab4bf514cca41c89046df2c4efee308db14390d
Mateusz Kula <me@mateuszkula.com>
2019-09-17 16:07:58 +0200

    Add Load More button on order history tab

--- src/@next/components/views/OrdersHistory/OrdersHistory.tsx
+++ src/@next/components/views/OrdersHistory/OrdersHistory.tsx
@@ -1,24 +1,43 @@
 import { useOrdersByUser } from "@sdk/react/";
 import React from "react";
 
-import { Loader } from "@components/atoms";
+import { Button, Loader } from "@components/atoms";
 import { OrderTabel } from "@components/molecules";
 
+import * as S from "./styles";
 import { IProps } from "./types";
 
+const ORDERS_PER_APICALL = 5;
+
 export const OrdersHistory: React.FC<IProps> = ({ history }: IProps) => {
-  const { data, loading } = useOrdersByUser(
+  const { data, loading, loadMore } = useOrdersByUser(
     {
-      perPage: 20,
+      perPage: ORDERS_PER_APICALL,
     },
     {
       fetchPolicy: "network-only",
     }
   );
 
-  return loading || !data ? (
+  return loading && !data ? (
     <Loader />
   ) : (
-    <OrderTabel orders={data.edges} history={history} />
+    <>
+      <OrderTabel orders={data!.edges} history={history} />
+      {data!.pageInfo.hasNextPage && (
+        <S.Wrapper>
+          <Button
+            onClick={() => {
+              loadMore({
+                after: data!.pageInfo.endCursor,
+                perPage: ORDERS_PER_APICALL,
+              });
+            }}
+          >
+            Load more
+          </Button>
+        </S.Wrapper>
+      )}
+    </>
   );
 };
