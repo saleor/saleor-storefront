1ae9dfd14a7329cb1b47551c881e52be848e48ba
macbook <macbook@MacBook-di-macbook.local>
2019-10-06 17:49:09 +0200

    apollo config changed

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: "saleor",
-      url: "http://localhost:8000/graphql/"
+      url: "http://storitalia.herokuapp.com/graphql/"
     }
   }
 };
