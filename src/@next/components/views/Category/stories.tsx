7907e17f60598f71a9fee4ed415e5a765acc7277
Mateusz Kula <me@mateuszkula.com>
2019-10-11 10:11:59 +0200

    Remove Category view

--- src/@next/components/views/Category/stories.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-
-import { Category } from ".";
-storiesOf("@components/views/Category", module)
-.add("default", () =>
-<Category />);
--- src/@next/components/views/Category/types.ts
+++
@@ -1,5 +0,0 @@
-import { RouteComponentProps } from "react-router";
-
-export type IProps = RouteComponentProps<{
-  id: string;
-}>;
