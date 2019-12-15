832c282c47c7d2fe00a2e4e8a2b83d93b9967b4b
Mirek Mencel <mirek@mirumee.com>
2019-12-15 17:07:48 +0100

    Update meta description and fix title to match the homepage's pattern

--- src/core/config.ts
+++ src/core/config.ts
@@ -38,10 +38,9 @@ export const SOCIAL_MEDIA = [
 ];
 export const META_DEFAULTS = {
   custom: [],
-  description: "PWA Storefront",
-
+  description: "Open-source PWA storefront built with Saleor's e-commerce GraphQL API. Written with React and TypeScript.",
   image: `${window.location.origin}${require("../images/logo.svg")}`,
-  title: "Saleor",
+  title: "Demo PWA Storefront – Saleor Commerce",
   type: "website",
   url: window.location.origin,
 };
