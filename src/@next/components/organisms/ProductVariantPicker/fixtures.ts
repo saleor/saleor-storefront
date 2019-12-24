20a7aee394437a0169e7de4e38728455d83cf71e
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2019-12-24 12:14:34 +0100

    Change product variant picker test

---
+++ src/@next/components/organisms/ProductVariantPicker/fixtures.ts
@@ -0,0 +1,263 @@
+import { ProductDetails_product_variants } from "@sdk/queries/types/ProductDetails";
+
+// tslint:disable:object-literal-sort-keys
+export const productVariants: ProductDetails_product_variants[] = [
+  {
+    id: "UHJvZHVjdFZhcmlhbnQ6MjA0",
+    sku: "80884671",
+    name: "2l / woolen / 910000ml",
+    stockQuantity: 50,
+    isAvailable: true,
+    pricing: {
+      onSale: false,
+      priceUndiscounted: {
+        gross: {
+          amount: 7,
+          currency: "USD",
+          localized: "$7.00",
+          __typename: "Money",
+        },
+        net: {
+          amount: 7,
+          currency: "USD",
+          localized: "$7.00",
+          __typename: "Money",
+        },
+        __typename: "TaxedMoney",
+      },
+      price: {
+        gross: {
+          amount: 7,
+          currency: "USD",
+          localized: "$7.00",
+          __typename: "Money",
+        },
+        net: {
+          amount: 7,
+          currency: "USD",
+          localized: "$7.00",
+          __typename: "Money",
+        },
+        __typename: "TaxedMoney",
+      },
+      __typename: "VariantPricingInfo",
+    },
+    attributes: [
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjI1",
+          name: "Material",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6ODY=",
+          name: "woolen",
+          value: "woolen",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjIw",
+          name: "Volume",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6ODQ=",
+          name: "910000ml",
+          value: "910000ml",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjE1",
+          name: "Size",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6NDg=",
+          name: "2l",
+          value: "2l",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+    ],
+    __typename: "ProductVariant",
+  },
+  {
+    id: "UHJvZHVjdFZhcmlhbnQ6MjAy",
+    sku: "93855755",
+    name: "1l / Cotton / 900000ml",
+    stockQuantity: 50,
+    isAvailable: true,
+    pricing: {
+      onSale: false,
+      priceUndiscounted: {
+        gross: {
+          amount: 3,
+          currency: "USD",
+          localized: "$3.00",
+          __typename: "Money",
+        },
+        net: {
+          amount: 3,
+          currency: "USD",
+          localized: "$3.00",
+          __typename: "Money",
+        },
+        __typename: "TaxedMoney",
+      },
+      price: {
+        gross: {
+          amount: 3,
+          currency: "USD",
+          localized: "$3.00",
+          __typename: "Money",
+        },
+        net: {
+          amount: 3,
+          currency: "USD",
+          localized: "$3.00",
+          __typename: "Money",
+        },
+        __typename: "TaxedMoney",
+      },
+      __typename: "VariantPricingInfo",
+    },
+    attributes: [
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjI1",
+          name: "Material",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6ODI=",
+          name: "Cotton",
+          value: "Cotton",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjIw",
+          name: "Volume",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6ODM=",
+          name: "900000ml",
+          value: "900000ml",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjE1",
+          name: "Size",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6NDc=",
+          name: "1l",
+          value: "1l",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+    ],
+    __typename: "ProductVariant",
+  },
+  {
+    id: "UHJvZHVjdFZhcmlhbnQ6MzE2",
+    sku: "100",
+    name: "500ml / woolen / 920000ml",
+    stockQuantity: 50,
+    isAvailable: true,
+    pricing: {
+      onSale: false,
+      priceUndiscounted: {
+        gross: {
+          amount: 12,
+          currency: "USD",
+          localized: "$12.00",
+          __typename: "Money",
+        },
+        net: {
+          amount: 12,
+          currency: "USD",
+          localized: "$12.00",
+          __typename: "Money",
+        },
+        __typename: "TaxedMoney",
+      },
+      price: {
+        gross: {
+          amount: 12,
+          currency: "USD",
+          localized: "$12.00",
+          __typename: "Money",
+        },
+        net: {
+          amount: 12,
+          currency: "USD",
+          localized: "$12.00",
+          __typename: "Money",
+        },
+        __typename: "TaxedMoney",
+      },
+      __typename: "VariantPricingInfo",
+    },
+    attributes: [
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjI1",
+          name: "Material",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6ODY=",
+          name: "woolen",
+          value: "woolen",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjIw",
+          name: "Volume",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6ODU=",
+          name: "920000ml",
+          value: "920000ml",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+      {
+        attribute: {
+          id: "QXR0cmlidXRlOjE1",
+          name: "Size",
+          __typename: "Attribute",
+        },
+        value: {
+          id: "QXR0cmlidXRlVmFsdWU6NDY=",
+          name: "500ml",
+          value: "500ml",
+          __typename: "AttributeValue",
+        },
+        __typename: "SelectedAttribute",
+      },
+    ],
+    __typename: "ProductVariant",
+  },
+];
--- src/@next/components/organisms/ProductVariantPicker/test.tsx
+++ src/@next/components/organisms/ProductVariantPicker/test.tsx
@@ -3,11 +3,13 @@ import "jest-styled-components";
 import React from "react";
 
 import { ProductVariantPicker } from ".";
+import { productVariants } from "./fixtures";
+
+const PROPS = { onChange: jest.fn(), productVariants };
 
 describe("<ProductVariantPicker />", () => {
-  // Example test
   it("exists", () => {
-    const wrapper = shallow(<ProductVariantPicker />);
+    const wrapper = shallow(<ProductVariantPicker {...PROPS} />);
 
     expect(wrapper.exists()).toEqual(true);
   });
