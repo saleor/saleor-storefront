e751cbd6845e6b308bc23d65c3f81b1463794788
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2019-12-27 14:04:13 +0100

    Upadate tests for ProductVariantPicker

--- src/@next/components/organisms/ProductVariantPicker/test.tsx
+++ src/@next/components/organisms/ProductVariantPicker/test.tsx
@@ -6,7 +6,7 @@ import { components } from "react-select";
 import { ProductVariantPicker } from ".";
 import { productVariants } from "./fixtures";
 
-const PROPS = { onChange: jest.fn(), productVariants };
+const PROPS = { productVariants };
 
 describe("<ProductVariantPicker />", () => {
   it("exists", () => {
@@ -15,8 +15,12 @@ describe("<ProductVariantPicker />", () => {
     expect(wrapper.exists()).toEqual(true);
   });
 
-  it("should disable possible selection of other variant attributes after selection of one variant attribute", () => {
-    const wrapper = mount(<ProductVariantPicker {...PROPS} />);
+  it("should select variant attribute value", () => {
+    const onChangeVariantPicker = jest.fn();
+
+    const wrapper = mount(
+      <ProductVariantPicker {...PROPS} onChange={onChangeVariantPicker} />
+    );
 
     wrapper
       .find("input")
@@ -29,6 +33,23 @@ describe("<ProductVariantPicker />", () => {
 
     expect(wrapper.text()).toContain("wool");
 
+    expect(onChangeVariantPicker).toHaveBeenCalled();
+  });
+
+  it("should disable possible selection of other variant attribute values after selection of one variant attribute values", () => {
+    const wrapper = mount(<ProductVariantPicker {...PROPS} />);
+
+    // Select value for first attribute
+    wrapper
+      .find("input")
+      .at(0)
+      .simulate("focus");
+    wrapper
+      .find(components.Option)
+      .at(1)
+      .simulate("click");
+
+    // Check if values are possible to select (disable or not) for another attribute
     wrapper
       .find("input")
       .at(1)
