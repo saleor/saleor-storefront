6a9f49aaefa21fee99c18211639f29af26b26d35
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-02-03 10:46:05 +0100

    Show snackbar add and remove to wishlist message

--- src/components/ProductDescription/AddToWishlist.tsx
+++ src/components/ProductDescription/AddToWishlist.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import { Message } from "@components/atoms";
 import { AddToWishlistButton } from "@components/molecules";
 import { useAddWishlistProduct, useRemoveWishlistProduct } from "@sdk/react";
 import { WishlistContext } from "@sdk/react/components/WishlistProvider/context";
 
 const AddToWishlist: React.FC<{ productId: string }> = ({ productId }) => {
   const { wishlist, update } = React.useContext(WishlistContext);
+
+  const [showAddMessage, setShowAddMessage] = React.useState(false);
+  const [showRemoveMessage, setShowRemoveMessage] = React.useState(false);
 
   const isAddedToWishlist = () => {
     return wishlist && wishlist.some(({ product }) => product.id === productId);
@@ -25,28 +29,64 @@ const AddToWishlist: React.FC<{ productId: string }> = ({ productId }) => {
 
   const [
     addWishlistProduct,
-    { data: addData, loading: addLoading, error: addError },
+    { loading: addLoading, error: addError },
   ] = useAddWishlistProduct({ productId });
   const [
     removeWishlistProduct,
-    { data: removeData, loading: removeLoading, error: removeError },
+    { loading: errorLoading, error: removeError },
   ] = useRemoveWishlistProduct({ productId });
 
   const addOrRemoveFromWishlist = async () => {
     if (addedToWishlist) {
-      await removeWishlistProduct({ productId });
+      removeWishlistProduct({ productId });
       update();
+      setShowAddMessage(false);
+      setShowRemoveMessage(true);
     } else {
       addWishlistProduct({ productId });
       update();
+      setShowRemoveMessage(false);
+      setShowAddMessage(true);
     }
   };
 
+  const handleRemoveMessageClose = () => {
+    setShowRemoveMessage(false);
+  };
+  const handleAddMessageClose = () => {
+    setShowAddMessage(false);
+  };
+
+  const getRemoveMessage = () =>
+    !addError
+      ? "Product removed from wishlist"
+      : "Error while removing product from wishlist";
+  const getAddMessage = () =>
+    !removeError
+      ? "Product added to wishlist"
+      : "Error while adding product to wishlist";
+
   return (
-    <AddToWishlistButton
-      added={addedToWishlist}
-      onClick={addOrRemoveFromWishlist}
-    />
+    <>
+      <AddToWishlistButton
+        added={addedToWishlist}
+        onClick={addOrRemoveFromWishlist}
+      />
+      {showAddMessage && !addLoading && (
+        <Message
+          title={getAddMessage()}
+          status={addError ? "error" : "success"}
+          onClick={handleAddMessageClose}
+        ></Message>
+      )}
+      {showRemoveMessage && !errorLoading && (
+        <Message
+          title={getRemoveMessage()}
+          status={removeError ? "error" : "success"}
+          onClick={handleRemoveMessageClose}
+        ></Message>
+      )}
+    </>
   );
 };
 
