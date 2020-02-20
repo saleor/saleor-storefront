ba38756fd1effaa38f52a77c3e3fc45ff20cd148
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-02-20 13:34:47 +0100

    Fix button storybook

--- src/@next/components/atoms/Button/stories.tsx
+++ src/@next/components/atoms/Button/stories.tsx
@@ -1,39 +1,21 @@
 import { boolean } from "@storybook/addon-knobs";
-// import { storiesOf } from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 import React from "react";
 
 import { Button } from ".";
 
-// storiesOf("@components/atoms/Button", module)
-//   .add("Primary", () => (
-//     <Button fullWidth={boolean("FullWidth", false)}>Primary Button</Button>
-//   ))
-//   .add("Secondary", () => (
-//     <Button color="secondary" fullWidth={boolean("FullWidth", false)}>
-//       Secondary Button
-//     </Button>
-//   ))
-//   .add("Size sm", () => (
-//     <Button size="sm" fullWidth={boolean("FullWidth", false)}>
-//       Small Button
-//     </Button>
-//   ));
-
-export default {
-  component: Button,
-  title: "@components/atoms/Button",
-};
-
-export const Primary = () => (
-  <Button fullWidth={boolean("FullWidth", false)}>Primary Button</Button>
-);
-export const Secondary = () => (
-  <Button color="secondary" fullWidth={boolean("FullWidth", false)}>
-    Secondary Button
-  </Button>
-);
-export const SizeSm = () => (
-  <Button size="sm" fullWidth={boolean("FullWidth", false)}>
-    Small Button
-  </Button>
-);
+storiesOf("@components/atoms/Button", module)
+  .addParameters({ component: Button })
+  .add("Primary", () => (
+    <Button fullWidth={boolean("FullWidth", false)}>Primary Button</Button>
+  ))
+  .add("Secondary", () => (
+    <Button color="secondary" fullWidth={boolean("FullWidth", false)}>
+      Secondary Button
+    </Button>
+  ))
+  .add("Size sm", () => (
+    <Button size="sm" fullWidth={boolean("FullWidth", false)}>
+      Small Button
+    </Button>
+  ));
