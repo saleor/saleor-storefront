13b1155ecd9d35ee5bb2eb5f0a8a3cccf92ad5d5
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-02-26 13:16:12 +0100

    Change imports of paths to routing components

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -2,10 +2,7 @@ import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { CheckoutLogin, NotFound } from "../../components";
-import UserAccount, {
-  baseUrl as userAccountBaseUrl,
-  userOrderDetailsUrl,
-} from "../../userAccount/routes";
+import UserAccount, * as accountPaths from "../../userAccount/routes";
 import { OrderDetails } from "../../userAccount/views";
 import { Account, AccountConfirm } from "../../views/Account";
 import { ArticlePage } from "../../views/Article";
@@ -19,45 +16,28 @@ import { SearchPage } from "../../views/Search";
 
 import { PasswordReset } from "../../@next/components/views";
 
-import {
-  accountConfirmUrl,
-  accountUrl,
-  addressBookUrl,
-  baseUrl,
-  cartUrl,
-  categoryUrl,
-  checkoutLoginUrl,
-  collectionUrl,
-  guestOrderDetailsUrl,
-  orderConfirmationUrl,
-  orderHistoryUrl,
-  pageUrl,
-  passwordResetUrl,
-  paymentOptionsUrl,
-  productUrl,
-  searchUrl,
-} from "./paths";
+import * as paths from "./paths";
 
 export const Routes: React.FC = () => (
   <Switch>
-    <Route exact path={baseUrl} component={HomePage} />
-    <Route path={searchUrl} component={SearchPage} />
-    <Route path={categoryUrl} component={CategoryPage} />
-    <Route path={collectionUrl} component={CollectionPage} />
-    <Route path={productUrl} component={ProductPage} />
-    <Route path={cartUrl} component={CartPage} />
-    <Route path={checkoutLoginUrl} component={CheckoutLogin} />
-    <Route path={pageUrl} component={ArticlePage} />
-    <Route path={userAccountBaseUrl} component={UserAccount} />
-    <Route path={userOrderDetailsUrl} component={OrderDetails} />
-    <Route path={guestOrderDetailsUrl} component={OrderDetails} />
-    <Route path={orderConfirmationUrl} component={OrderConfirmation} />
-    <Route path={accountUrl} component={Account} />
-    <Route path={accountConfirmUrl} component={AccountConfirm} />
-    <Route path={orderHistoryUrl} component={Account} />
-    <Route path={addressBookUrl} component={Account} />
-    <Route path={paymentOptionsUrl} component={Account} />
-    <Route path={passwordResetUrl} component={PasswordReset} />
+    <Route exact path={paths.baseUrl} component={HomePage} />
+    <Route path={paths.searchUrl} component={SearchPage} />
+    <Route path={paths.categoryUrl} component={CategoryPage} />
+    <Route path={paths.collectionUrl} component={CollectionPage} />
+    <Route path={paths.productUrl} component={ProductPage} />
+    <Route path={paths.cartUrl} component={CartPage} />
+    <Route path={paths.checkoutLoginUrl} component={CheckoutLogin} />
+    <Route path={paths.pageUrl} component={ArticlePage} />
+    <Route path={accountPaths.baseUrl} component={UserAccount} />
+    <Route path={accountPaths.userOrderDetailsUrl} component={OrderDetails} />
+    <Route path={paths.guestOrderDetailsUrl} component={OrderDetails} />
+    <Route path={paths.orderConfirmationUrl} component={OrderConfirmation} />
+    <Route path={paths.accountUrl} component={Account} />
+    <Route path={paths.accountConfirmUrl} component={AccountConfirm} />
+    <Route path={paths.orderHistoryUrl} component={Account} />
+    <Route path={paths.addressBookUrl} component={Account} />
+    <Route path={paths.paymentOptionsUrl} component={Account} />
+    <Route path={paths.passwordResetUrl} component={PasswordReset} />
     <Route component={NotFound} />
   </Switch>
 );
--- src/checkout/routes/CheckoutRoutes.tsx
+++ src/checkout/routes/CheckoutRoutes.tsx
@@ -5,23 +5,16 @@ import { NotFound } from "../../components";
 import { Billing, Payment, Review, Shipping, ShippingOptions } from "../views";
 import { CheckoutRouteDispatcher } from "./CheckoutRouteDispatcher";
 
-import {
-  baseUrl,
-  billingUrl,
-  paymentUrl,
-  reviewUrl,
-  shippingAddressUrl,
-  shippingOptionsUrl,
-} from ".";
+import * as paths from ".";
 
 export const CheckoutRoutes: React.FC = () => (
   <Switch>
-    <Route exact path={baseUrl} component={CheckoutRouteDispatcher} />
-    <Route path={shippingAddressUrl} component={Shipping} />
-    <Route path={shippingOptionsUrl} component={ShippingOptions} />
-    <Route path={billingUrl} component={Billing} />
-    <Route path={paymentUrl} component={Payment} />
-    <Route path={reviewUrl} component={Review} />
+    <Route exact path={paths.baseUrl} component={CheckoutRouteDispatcher} />
+    <Route path={paths.shippingAddressUrl} component={Shipping} />
+    <Route path={paths.shippingOptionsUrl} component={ShippingOptions} />
+    <Route path={paths.billingUrl} component={Billing} />
+    <Route path={paths.paymentUrl} component={Payment} />
+    <Route path={paths.reviewUrl} component={Review} />
     <Route component={NotFound} />
   </Switch>
 );
