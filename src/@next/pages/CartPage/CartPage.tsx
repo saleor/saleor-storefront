a4980128c34fdf77248ad732cb239ed456139fe2
Mateusz Kula <me@mateuszkula.com>
2020-03-10 13:34:49 +0100

    Cleanup CartPage component for now

--- src/@next/pages/CartPage/CartPage.tsx
+++ src/@next/pages/CartPage/CartPage.tsx
@@ -1,45 +1,9 @@
 import React, { useEffect } from "react";
 
 import { Cart } from "@components/templates";
-import { useCheckout } from "@sdk/react";
 
 import { IProps } from "./types";
 
 export const CartPage: React.FC<IProps> = ({}: IProps) => {
-  const {
-    checkout,
-    load,
-    loading,
-    errors,
-    updateItemInCart,
-    removeItemFromCart,
-  } = useCheckout();
-
-  useEffect(() => {
-    if (!checkout) {
-      load();
-    }
-  }, []);
-
-  const isLoading = ({
-    load,
-    updateItemInCart,
-    removeItemFromCart,
-  }: typeof loading) => {
-    return load || updateItemInCart || removeItemFromCart;
-  };
-
-  const checkoutItems = checkout?.lines || [];
-
-  return (
-    <>
-      <Cart
-        items={checkoutItems}
-        updateItem={updateItemInCart}
-        removeItem={removeItemFromCart}
-        loading={isLoading(loading)}
-        errors={errors}
-      />
-    </>
-  );
+  return <Cart />;
 };
