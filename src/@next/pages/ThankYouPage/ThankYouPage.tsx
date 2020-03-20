29ddcf24a2ace104ff137ef1ddfe2a12d05a6e1d
Mateusz Kula <me@mateuszkula.com>
2020-03-20 18:09:16 +0100

    Add logic to ThankYouPage

--- src/@next/pages/ThankYouPage/ThankYouPage.tsx
+++ src/@next/pages/ThankYouPage/ThankYouPage.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import { baseUrl } from "../../../app/routes/paths";
+
+import { NotFound } from "../../../components";
 
 import { IProps } from "./types";
+import { ThankYou } from "../../components/organisms";
 
 const ThankYouPage: React.FC<IProps> = ({}: IProps) => {
-  return <p>hi</p>;
+  const location = useLocation();
+  const history = useHistory();
+  const { token, id, orderNumber } = location.state;
+  if (!token || !id) {
+    return <NotFound />;
+  }
+  return (
+    <ThankYou
+      continueShopping={() => history.push(baseUrl)}
+      orderNumber={orderNumber}
+      orderDetails={() => {}}
+    />
+  );
 };
 
 export { ThankYouPage };
