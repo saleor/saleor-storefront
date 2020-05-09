d5822cfbb12e43f11cafb57665a403280023e9b1
WaelMohamedKhelil <khelilwael@gmail.com>
2020-05-09 23:55:52 +0100

    configure react-intl

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,40 @@
-module.exports = {
-  presets: [
+module.exports = api => {
+  const isExtract = api.env("extract");
+  const isTest = api.env("test");
+  const isStorybook = api.env("storybook");
+
+  const ignore =
+    isTest || isStorybook
+      ? []
+      : ["**/*.test.ts", "**/*.test.tsx", "src/storybook"];
+  const presets = [
     "@babel/preset-env",
     "@babel/preset-react",
     "@babel/preset-typescript",
-  ],
-  plugins: [
+  ]
+  const plugins = [
     "@babel/plugin-syntax-dynamic-import",
     "transform-class-properties",
     "@babel/transform-runtime",
     "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-object-rest-spread",
+    "react-intl-auto"
   ],
+  if (isExtract) {
+    plugins.push([
+      "react-intl",
+      {
+        extractFromFormatMessageCall: true,
+        messagesDir: "dist/locale/"
+      }
+    ]);
+  }
+
+  plugins.push("macros");
+
+  return {
+    ignore,
+    plugins,
+    presets
+  };
 };
