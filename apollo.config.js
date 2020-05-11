8f7c89405abfa50219ab2e0138fe18e2dbd55302
dominik-zeglen <flesz3@o2.pl>
2020-05-11 11:39:48 +0200

    Separate core and sdk codegen

--- apollo.config.js
+++ apollo.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   client: {
+    excludes: ["**/__tests__/**/*", "**/@sdk/**/*"],
     service: {
       name: "saleor",
       url: "http://localhost:8000/graphql/",
-      includes: ["./**/*.js", "./**/*.ts"],
-      excludes: ["**/__tests__/**/*"],
     },
   },
 };
