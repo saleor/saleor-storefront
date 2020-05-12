bb9cfb0401d2c1ab80d1acd7b96db851538719ce
WaelMohamedKhelil <khelilwael@gmail.com>
2020-05-12 15:42:15 +0100

    refactor table

--- src/components/CartTable/Table.tsx
+++ src/components/CartTable/Table.tsx
@@ -7,6 +7,7 @@ import Media from "react-media";
 import CostRow from "./CostRow";
 import ProductRow, { EditableProductRowProps, ILine } from "./ProductRow";
 
+import { useIntl } from "react-intl";
 interface TableProps extends EditableProductRowProps {
   lines: ILine[];
   subtotal: React.ReactNode;
@@ -24,17 +25,52 @@ const Table: React.FC<TableProps> = ({
   discountName,
   lines,
   ...rowProps
-}) => (
+}) => {
+  const intl = useIntl();
+
+  return(
   <Media query={{ minWidth: smallScreen }}>
     {mediumScreen => (
       <table className="cart-table">
         <thead>
           <tr>
-            <th>Products</th>
-            {mediumScreen && <th>Price</th>}
-            <th>Variant</th>
-            <th className="cart-table__quantity-header">Quantity</th>
-            <th colSpan={2}>{mediumScreen ? "Total Price" : "Price"}</th>
+            <th> 
+              {
+                intl.formatMessage({
+                defaultMessage: "Products",
+                description: "product table th",
+              })}
+              </th>
+            {mediumScreen && <th>
+              {
+                intl.formatMessage({
+                defaultMessage: "Price",
+                description: "price table th",
+              })}
+              </th>}
+            <th>
+              {
+                intl.formatMessage({
+                defaultMessage: "Variant",
+                description: "variant table th",
+              })}
+              </th>
+            <th className="cart-table__quantity-header">
+              {
+                intl.formatMessage({
+                defaultMessage: "Quantity",
+                description: "quantity table th",
+              })}
+            </th>
+            <th colSpan={2}>{mediumScreen ? 
+            intl.formatMessage({
+              defaultMessage: "Total Price",
+              description: "total price table th",
+            }) : 
+            intl.formatMessage({
+              defaultMessage: "Price",
+              description: "price table th",
+            })}</th>
           </tr>
         </thead>
         <tbody>
@@ -50,27 +86,44 @@ const Table: React.FC<TableProps> = ({
         <tfoot>
           <CostRow
             mediumScreen={mediumScreen}
-            heading="Subtotal"
+            heading={intl.formatMessage({
+              defaultMessage: "Subtotal",
+              description: "Subtotal table t row",
+            })}
             cost={subtotal}
           />
           {discount && (
             <CostRow
               mediumScreen={mediumScreen}
-              heading={`Discount: ${discountName}`}
+              heading=
+              {intl.formatMessage({
+                defaultMessage: "Discount: {discountName}",
+                description: "Discount table t row",
+              },{
+                discountName,
+              })}
               cost={discount}
             />
           )}
           {deliveryCost && (
             <CostRow
               mediumScreen={mediumScreen}
-              heading="Delivery Cost"
+              heading=
+              {intl.formatMessage({
+                defaultMessage: "Delivery Cost",
+                description: "delivery cost table t row",
+              })}
               cost={deliveryCost}
             />
           )}
           {totalCost && (
             <CostRow
               mediumScreen={mediumScreen}
-              heading="Total Cost"
+              heading=
+              {intl.formatMessage({
+                defaultMessage: "Total Cost",
+                description: "total cost table t row",
+              })}
               cost={totalCost}
             />
           )}
@@ -78,6 +131,6 @@ const Table: React.FC<TableProps> = ({
       </table>
     )}
   </Media>
-);
+)}
 
 export default Table;
