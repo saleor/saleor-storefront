41deb7ddf9bc480b5a1243747e3119eac592b36b
WaelMohamedKhelil <khelilwael@gmail.com>
2020-05-13 16:12:42 +0100

    not found page refactor

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import { BASE_URL } from "../../core/config";
 import Button from "../Button";
 
+import { FormattedMessage } from "react-intl";
+
 interface NotFoundProps {
   message?: string;
 }
@@ -14,13 +16,33 @@ const NotFound: React.FC<NotFoundProps> = () => (
     <h2 className="not-found-page__header">404</h2>
     <div className="not-found-page__ruler" />
     <div className="not-found-page__message">
-      <p>We can’t seem to find a page you are looking for! </p>
-      <p>You may have mistyped the address or the page may have moved. </p>
-      <p>We’re sorry for the error and hope you’ll have a good day.</p>
+      <p>
+        <FormattedMessage
+          defaultMessage={"We can’t seem to find a page you are looking for!"}
+          description={"not found page message"}
+        />
+      </p>
+      <p>
+        <FormattedMessage
+          defaultMessage={"You may have mistyped the address or the page may have moved."}
+          description={"not found page message"}
+        />
+      </p>
+      <p>
+        <FormattedMessage
+          defaultMessage={"We’re sorry for the error and hope you’ll have a good day."}
+          description={"not found page message"}
+        />
+      </p>
     </div>
     <div className="not-found-page__button">
       <Link to={BASE_URL}>
-        <Button secondary>Back to home</Button>
+        <Button secondary>
+          <FormattedMessage
+            defaultMessage={"Back to home"}
+            description={"not found page button"}
+          />
+        </Button>
       </Link>
     </div>
   </div>
