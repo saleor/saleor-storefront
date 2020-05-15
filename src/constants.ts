49fd348f120ad92d8036346e2dcb3352e7178df6
Patryk Zawadzki <patrys@room-303.com>
2020-05-15 15:00:38 +0200

    Add support for Sentry and performance tracing

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
 export const apiUrl = process.env.API_URI;
+export const sentryDsn = process.env.SENTRY_DSN;
+const sampleRate = parseFloat(process.env.SENTRY_APM);
+export const sentrySampleRate = isNaN(sampleRate) ? 0 : sampleRate;
 export const serviceWorkerTimeout =
   parseInt(process.env.SERVICE_WORKER_TIMEOUT, 10) || 60 * 1000;
