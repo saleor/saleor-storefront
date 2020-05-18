86bad9a03ae862b6caae20d5ef27a3fb278350da
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-05-18 12:14:53 +0200

    Add docstring to LocalStorageHandlerProxy

--- src/@sdk/helpers/LocalStorageHandler/LocalStorageHandler.ts
+++ src/@sdk/helpers/LocalStorageHandler/LocalStorageHandler.ts
@@ -6,7 +6,17 @@ import {
   LocalStorageItems,
 } from "./types";
 
+/**
+ * Sets or removes data from local storage in one of the specified data format.
+ * If data is set to null, then it is removed from local storage.
+ * If needed, it stringify data for persistance in local storage or parse such data to be retrieved in desired format.
+ */
 class LocalStorageHandlerProxy extends NamedObservable<LocalStorageItems> {
+  /**
+   * Save string item to local storage.
+   * @param name Unique key by which item is identified.
+   * @param item String to be saved. If null, then item is completely removed from local storage.
+   */
   protected saveItem(name: LocalStorageItems, item: string | null): void {
     if (item) {
       localStorage.setItem(name, item);
@@ -15,9 +25,18 @@ class LocalStorageHandlerProxy extends NamedObservable<LocalStorageItems> {
     }
     this.notifyChange(name, item);
   }
+  /**
+   * Retrieve string item from local storage.
+   * @param name Unique key by which item is identified.
+   */
   protected retrieveItem(name: LocalStorageItems): string | null {
     return localStorage.getItem(name);
   }
+  /**
+   * Stringify object and saves it to local storage.
+   * @param name Unique key by which object is identified.
+   * @param item Object to be saved. If null, then object is completely removed from local storage.
+   */
   protected saveObject<T extends object>(
     name: LocalStorageItems,
     object: T | null
@@ -29,6 +48,10 @@ class LocalStorageHandlerProxy extends NamedObservable<LocalStorageItems> {
     }
     this.notifyChange(name, object);
   }
+  /**
+   * Retrieve item from local storage and parse it as object.
+   * @param name Unique key by which object is identified.
+   */
   protected retrieveObject<T extends object>(
     name: LocalStorageItems
   ): T | null {
