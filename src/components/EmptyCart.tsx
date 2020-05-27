96cce33da83a10691c9365ca5ba77e94fdcfa735
nikunjsdev <58289663+nikunjsdev@users.noreply.github.com>
2020-05-27 09:40:26 +0530

    Add message on samples page and redesign empty cart page (#9)
    * Add message on samples page and redesign empty cart [age
    
    * empty cart background image design
    
    * Rename variable change box-shadow to variable

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -1,22 +1,45 @@
+import arrowLeftIcon from "images/arrow-left.svg";
 import * as React from "react";
 import { Link } from "react-router-dom";
+import ReactSVG from "react-svg";
+import cartIcon from "../images/cart-empty.svg";
+import crossIcon from "../images/cross-clear.svg";
+import pinkBackground from "../images/pink-background.svg";
 
 import { baseUrl } from "../app/routes";
 import Button from "./Button";
 
-const EmptyCart: React.FC<{}> = () => (
-  <div className="cart-page__empty">
-    <h4>Your bag is empty</h4>
-    <p>
-      You haven’t added anything to your bag. We’re sure you’ll find something
-      in our store
-    </p>
-    <div className="cart-page__empty__action">
-      <Link to={baseUrl}>
-        <Button secondary>Continue Shopping</Button>
-      </Link>
+const EmptyCart: React.FC<{}> = () => {
+  const handleBack = () => {
+    window.history.back();
+  };
+
+  return (
+    <div className="cart-page__empty inner-page-wrapper">
+      <div className="cart-page__empty-close">
+        <ReactSVG
+          path={crossIcon}
+          className="empty-icon"
+          onClick={handleBack}
+        />
+      </div>
+      <div
+        className="cart-page__empty-icon"
+        style={{ backgroundImage: `url(${pinkBackground})` }}
+      >
+        <div className="cart-page__empty-icon-circle">
+          <ReactSVG path={cartIcon} className="empty-icon" />
+        </div>
+      </div>
+      <h4>Your cart is empty</h4>
+      <p>You haven’t added anything yet.</p>
+      <div className="cart-page__empty__action">
+        <Link to={baseUrl}>
+          <Button className="home-page__btn">Continue Shopping</Button>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default EmptyCart;
--- src/views/CollectionIndex/Samples/Page.tsx
+++ src/views/CollectionIndex/Samples/Page.tsx
@@ -12,6 +12,7 @@ interface IPage {
 export const Page = ({ data, cart, collectionId, history }: IPage) => {
   const [samples, selectedSample] = useState([]);
   const collect = [];
+  const sampleExist = data.products.totalCount;
 
   const handleBack = () => {
     history.goBack();
@@ -49,49 +50,61 @@ export const Page = ({ data, cart, collectionId, history }: IPage) => {
       <div>
         <div className="wrapper-header">Samples</div>
         <div className="wrapper-img">
-          {data.products.edges.map(
-            ({ node: { name, id, pricing, thumbnail, variants } }, idx) => (
-              <div className="wrapper-img-main" key={idx}>
-                <div
-                  className={
-                    samples
-                      ? samples.indexOf(variants[0].id) > -1
-                        ? "wrapper-img-main-inner withBorder"
-                        : "wrapper-img-main-inner noBorder"
-                      : ""
-                  }
-                >
-                  <div className="wrapper-img-main-inner--header">
-                    <span>{name}</span>
-                  </div>
-                  <div className="wrapper-img-main-inner--img">
-                    <img
-                      src={thumbnail.url}
-                      onClick={() => selectSample(variants[0].id)}
-                      id={id}
-                      key={idx}
-                    />
-                  </div>
-                  <div className="wrapper-img-main-inner--price">
-                    <span className="old-price">
-                      <del>${pricing.priceRange.start.gross.amount}</del>
-                    </span>
-                    <span className="new-price">
-                      ${pricing.priceRange.start.net.amount}
-                    </span>
+          {sampleExist > 0 ? (
+            data.products.edges.map(
+              ({ node: { name, id, pricing, thumbnail, variants } }, idx) => (
+                <div className="wrapper-img-main" key={idx}>
+                  <div
+                    className={
+                      samples
+                        ? samples.indexOf(variants[0].id) > -1
+                          ? "wrapper-img-main-inner withBorder"
+                          : "wrapper-img-main-inner noBorder"
+                        : ""
+                    }
+                  >
+                    <div className="wrapper-img-main-inner--header">
+                      <span>{name}</span>
+                    </div>
+                    <div className="wrapper-img-main-inner--img">
+                      <img
+                        src={thumbnail.url}
+                        onClick={() => selectSample(variants[0].id)}
+                        id={id}
+                        key={idx}
+                      />
+                    </div>
+                    <div className="wrapper-img-main-inner--price">
+                      {pricing.priceRange.start.gross.amount !==
+                        pricing.priceRange.start.net.amount && (
+                        <span className="old-price">
+                          <del>${pricing.priceRange.start.gross.amount}</del>
+                        </span>
+                      )}
+                      <span className="new-price">
+                        ${pricing.priceRange.start.net.amount}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
+              )
             )
+          ) : (
+            <div className="wrapper-coming-soon">
+              <span className="text">There are no samples...</span>
+            </div>
           )}
         </div>
-        <button
-          type="button"
-          className="home-page__btn"
-          onClick={handleOrderSamples}
-        >
-          Order Samples
-        </button>
+
+        {sampleExist > 0 && (
+          <button
+            type="button"
+            className="home-page__btn"
+            onClick={handleOrderSamples}
+          >
+            Order Samples
+          </button>
+        )}
       </div>
     </div>
   );
