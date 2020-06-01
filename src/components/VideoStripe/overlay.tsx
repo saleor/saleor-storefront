d76e739eb56d337af3578f4c69d59b4029df58b7
Callum Ormsby <callum.ormsby@gmail.com>
2020-06-01 16:28:46 +0100

    Add Link To Button Press

--- src/components/VideoStripe/overlay.tsx
+++ src/components/VideoStripe/overlay.tsx
@@ -5,7 +5,9 @@ const overlay = props => (
     <div className={classes.InfoCard}>
         <div className={classes.Title}>{props.title}</div>
         <div className={classes.Subtitle}>{props.subtitle}</div>
-        <button className={classes.Button}>Youtube page</button>
+        <button
+            className={classes.Button}
+            onClick={() => location.href = props.buttonDestination}>Youtube page</button>
     </div>
 );
 
