d074ee91b07332d1eaa5e8db0ed9ebd65f5f31a1
dominik-zeglen <flesz3@o2.pl>
2020-06-10 12:28:27 +0200

    Add missing dependencies

--- config/webpack/config.base.js
+++ config/webpack/config.base.js
@@ -11,39 +11,20 @@ if (!process.env.API_URI) {
 }
 
 module.exports = ({ sourceDir, distDir }) => ({
-  resolve: {
-    alias: {
-      // Explicitely set react's path here because npm-link doesn't do well
-      // when it comes to peer dependencies, and we need to somehow develop
-      // @saleor/sdk package
-      react: path.resolve("./node_modules/react"),
-      "react-dom": "@hot-loader/react-dom",
-    },
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-    plugins: [
-      new TsconfigPathsPlugin({
-        configFile: "./tsconfig.json",
-      }),
-    ],
-  },
+  devtool: "source-map",
   entry: {
     app: `${sourceDir}/index.tsx`,
   },
-  output: {
-    path: distDir,
-    publicPath: "/",
-  },
-  devtool: "source-map",
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
         exclude: /node_modules/,
+        loader: "ts-loader",
         options: {
           experimentalWatchApi: true,
           transpileOnly: true,
         },
+        test: /\.tsx?$/,
       },
       {
         test: /\.(woff2?|ttf|eot)$/,
@@ -72,6 +53,9 @@ module.exports = ({ sourceDir, distDir }) => ({
           {
             loader: "image-webpack-loader",
             options: {
+              gifsicle: {
+                enabled: false,
+              },
               mozjpeg: {
                 progressive: true,
                 quality: 85,
@@ -80,51 +64,67 @@ module.exports = ({ sourceDir, distDir }) => ({
                 quality: "65-90",
                 speed: 4,
               },
-              gifsicle: {
-                enabled: false,
-              },
             },
           },
         ],
       },
     ],
   },
+  node: {
+    fs: "empty",
+    module: "empty",
+  },
+  output: {
+    path: distDir,
+    publicPath: "/",
+  },
   plugins: [
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: distDir,
     }),
     new HtmlWebpackPlugin({
+      API_URI: process.env.API_URI,
       filename: `${distDir}/index.html`,
       template: `${sourceDir}/index.html`,
-      API_URI: process.env.API_URI,
     }),
     new ForkTsCheckerWebpackPlugin({
-      tslint: true,
+      eslint: true,
       exclude: "node_modules",
     }),
     // PWA plugins
     new WebappWebpackPlugin({
-      logo: `${sourceDir}/images/favicon.png`,
-      prefix: "images/favicons/",
       favicons: {
-        appName: "Saleor",
         appDescription: "Storefront for the Saleor e-commerce platform",
-        display: "standalone",
-        developerURL: null, // prevent retrieving from the nearest package.json
+        appName: "Saleor",
         background: "#ddd",
+        developerURL: null, // prevent retrieving from the nearest package.json
+        display: "standalone",
         theme_color: "#333",
       },
+      logo: `${sourceDir}/images/favicon.png`,
+      prefix: "images/favicons/",
     }),
     new webpack.EnvironmentPlugin({
       API_URI: "http://localhost:8000/graphql/",
-      SENTRY_DSN: null,
-      SENTRY_APM: "0",
       DEMO_MODE: false,
       GTM_ID: undefined,
+      SENTRY_APM: "0",
+      SENTRY_DSN: null,
     }),
   ],
-  node: {
-    fs: "empty",
-    module: "empty",
+  resolve: {
+    alias: {
+      // Explicitely set react's path here because npm-link doesn't do well
+      // when it comes to peer dependencies, and we need to somehow develop
+      // @saleor/sdk package
+      react: path.resolve("./node_modules/react"),
+      "react-dom": "@hot-loader/react-dom",
+    },
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: "./tsconfig.json",
+      }),
+    ],
   },
 });
