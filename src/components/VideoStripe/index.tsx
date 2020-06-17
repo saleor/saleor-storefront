a2c7816bdfe6d2bc1c5a1d82eddf7c3592ff9163
Callum Ormsby <callum.ormsby@gmail.com>
2020-06-17 12:50:47 +0100

    Replace Youtube Button With Wrapping Hyperlink

--- src/components/VideoStripe/index.tsx
+++ src/components/VideoStripe/index.tsx
@@ -3,19 +3,21 @@ import classes from './scss/index.module.scss';
 import Overlay from './overlay';
 
 const videoStripe = props => (
-    <div className={classes.VideoBackground}>
-        <div className={classes.VideoForeground}>
-            <Overlay title="V12 Athlete Craig" subtitle="Swiss Summer '19" buttonDestination="https://youtube.com" />
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://www.youtube-nocookie.com/embed/QVkZcHPHkc8?controls=0&autoplay=1&modestbranding=1&mute=1&loop=1&playlist=QVkZcHPHkc8"
-                frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen>
-            </iframe>
+    <a href="https://youtube.com" target="_blank">
+        <div className={classes.VideoBackground}>
+            <div className={classes.VideoForeground}>
+                <Overlay title="V12 Athlete Craig" subtitle="Swiss Summer '19" />
+                <iframe
+                    width="100%"
+                    height="100%"
+                    src="https://www.youtube-nocookie.com/embed/QVkZcHPHkc8?controls=0&autoplay=1&modestbranding=1&mute=1&loop=1&playlist=QVkZcHPHkc8"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen>
+                </iframe>
+            </div>
         </div>
-    </div>
+    </a>
 );
 
 export default videoStripe;
--- src/components/VideoStripe/overlay.tsx
+++ src/components/VideoStripe/overlay.tsx
@@ -5,9 +5,6 @@ const overlay = props => (
     <div className={classes.InfoCard}>
         <div className={classes.Title}>{props.title}</div>
         <div className={classes.Subtitle}>{props.subtitle}</div>
-        <button
-            className={classes.Button}
-            onClick={() => location.href = props.buttonDestination}>Youtube page</button>
     </div>
 );
 
