ffb36f61e16367d356c7951db6b5404e21ca212c
Przemysław Łada <przlada@gmail.com>
2020-06-23 00:46:38 +0200

    Add default messages

---
+++ src/intl.ts
@@ -0,0 +1,109 @@
+import { defineMessages } from "react-intl";
+
+export const commonMessages = defineMessages({
+  search: {
+    defaultMessage: "search",
+  },
+  outOfStock: {
+    defaultMessage: "Out of stock",
+  },
+  lowStock: {
+    defaultMessage: "Low stock",
+  },
+  myAccount: {
+    defaultMessage: "My Account",
+  },
+  orderHistory: {
+    defaultMessage: "Order history",
+  },
+  addressBook: {
+    defaultMessage: "Address book",
+  },
+  logOut: {
+    defaultMessage: "Log Out",
+  },
+  firstName: {
+    defaultMessage: "First Name",
+  },
+  lastName: {
+    defaultMessage: "Last Name",
+  },
+  password: {
+    defaultMessage: "Password",
+  },
+  quantity: {
+    defaultMessage: "Quantity",
+  },
+  sku: {
+    defaultMessage: "SKU",
+  },
+});
+
+export const checkoutMessages = defineMessages({
+  stepNameAddress: {
+    defaultMessage: "Address",
+  },
+  stepNameShipping: {
+    defaultMessage: "Shipping",
+  },
+  stepNamePayment: {
+    defaultMessage: "Payment",
+  },
+  stepNameReview: {
+    defaultMessage: "Review",
+  },
+  addressNextActionName: {
+    defaultMessage: "Continue to Shipping",
+  },
+  shippingNextActionName: {
+    defaultMessage: "Continue to Payment",
+  },
+  paymentNextActionName: {
+    defaultMessage: "Continue to Review",
+  },
+  reviewNextActionName: {
+    defaultMessage: "Place order",
+  },
+  addNewAddress: {
+    defaultMessage: "Add new address",
+  },
+  shippingMethod: {
+    defaultMessage: "SHIPPING METHOD",
+  },
+  billingAddress: {
+    defaultMessage: "BILLING ADDRESS",
+  },
+  paymentMethod: {
+    defaultMessage: "PAYMENT METHOD",
+  },
+  reviewOrder: {
+    defaultMessage: "REVIEW ORDER",
+  },
+  shippingAddress: {
+    defaultMessage: "Shipping Address",
+  },
+});
+
+export const prodListHeaderCommonMsg = defineMessages({
+  sortOptionsClear: {
+    defaultMessage: "Clear...",
+  },
+  sortOptionsPrice: {
+    defaultMessage: "Price Low-High",
+  },
+  sortOptionsPriceDsc: {
+    defaultMessage: "Price High-Low",
+  },
+  sortOptionsName: {
+    defaultMessage: "Name Increasing",
+  },
+  sortOptionsNameDsc: {
+    defaultMessage: "Name Decreasing",
+  },
+  sortOptionsUpdatedAt: {
+    defaultMessage: "Last updated Ascending",
+  },
+  sortOptionsUpdatedAtDsc: {
+    defaultMessage: "Last updated Descending",
+  },
+});
