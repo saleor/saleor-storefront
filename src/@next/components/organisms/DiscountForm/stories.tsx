d57246033e77dddec81bb271f196862bb3fd43b1
Przemysław Łada <przlada@gmail.com>
2020-06-26 23:15:09 +0200

    Localize DiscountForm tests

--- src/@next/components/organisms/DiscountForm/stories.tsx
+++ src/@next/components/organisms/DiscountForm/stories.tsx
@@ -1,8 +1,13 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
+import { IntlProvider } from "react-intl";
 
 import { DiscountForm } from ".";
 
 storiesOf("@components/organisms/DiscountForm", module)
   .addParameters({ component: DiscountForm })
-  .add("default", () => <DiscountForm />);
+  .add("default", () => (
+    <IntlProvider locale="en">
+      <DiscountForm />
+    </IntlProvider>
+  ));
