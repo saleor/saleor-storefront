738b5500aeba0c80175a808c8099ace23f62d08b
Przemysław Łada <przlada@gmail.com>
2020-06-26 23:15:09 +0200

    Localize DiscountForm tests

--- src/@next/components/organisms/DiscountForm/test.tsx
+++ src/@next/components/organisms/DiscountForm/test.tsx
@@ -1,12 +1,17 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import "jest-styled-components";
 import React from "react";
+import { IntlProvider } from "react-intl";
 
 import { DiscountForm } from ".";
 
 describe("<DiscountForm />", () => {
   it("exists", () => {
-    const wrapper = shallow(<DiscountForm />);
+    const wrapper = mount(
+      <IntlProvider locale="en">
+        <DiscountForm />
+      </IntlProvider>
+    );
 
     expect(wrapper.exists()).toEqual(true);
   });
