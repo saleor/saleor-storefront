0b0842f25ef88d181d303b7de15099317c6b3ae0
dominik-zeglen <flesz3@o2.pl>
2020-06-29 15:09:31 +0200

    Do not store passwords in localStorage

--- src/@sdk/api/types.ts
+++ src/@sdk/api/types.ts
@@ -1,3 +1,8 @@
+import { ApolloError } from "apollo-client";
+import { PasswordChange } from "../mutations/gqlTypes/PasswordChange";
+import { SetPassword } from "../mutations/gqlTypes/SetPassword";
+import { TokenAuth_tokenCreate } from "../mutations/gqlTypes/TokenAuth";
+
 export interface ErrorResponse<T> {
   error?: any;
   type?: T;
@@ -15,3 +20,18 @@ export interface FunctionRunResponse<D, F> {
 
 export type PromiseQueuedResponse = Promise<FunctionQueueResponse>;
 export type PromiseRunResponse<D, F> = Promise<FunctionRunResponse<D, F>>;
+
+export type SignIn = {
+  data: TokenAuth_tokenCreate | null;
+  error: ApolloError | null;
+} | null;
+
+export type SetPasswordChange = {
+  data: PasswordChange | null;
+  error: ApolloError | null;
+} | null;
+
+export type SetPasswordResult = {
+  data: SetPassword | null;
+  error: ApolloError | null;
+} | null;
