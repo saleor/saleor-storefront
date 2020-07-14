33fcbd1caf3574606e50c08c21d9cf8a880fff0a
Michalina Graczyk <michalina.graczyk@mirumee.com>
2020-07-14 11:09:01 +0200

    changed email and pass in valid_user file

--- cypress/support/category/index.js
+++ cypress/support/category/index.js
@@ -1,18 +1,12 @@
-
 const openFilterSidebar = () => {
-  const filterButton = "[data-test=filtersButton]"
+  const filterButton = "[data-test=filtersButton]";
 
-  return cy
-    .get(filterButton)
-    .click();
+  return cy.get(filterButton).click();
 };
 
 const openCategory = (index = 0) => {
-  const leftMainMenu = "[data-test=mainMenuItem]"
-  return cy
-    .get(leftMainMenu, { timeout: 5000 })
-    .eq(index)
-    .click();
+  const leftMainMenu = "[data-test=mainMenuItem]";
+  return cy.get(leftMainMenu, { timeout: 5000 }).eq(index).click();
 };
 
 Cypress.Commands.add("openFilterSidebar", openFilterSidebar);
