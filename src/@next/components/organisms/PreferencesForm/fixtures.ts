85b53278c3e175717b8c78eb251782485d296feb
Przemysław Łada <przlada@gmail.com>
2020-07-31 01:19:15 +0200

    Add PreferencesForm tests

---
+++ src/@next/components/organisms/PreferencesForm/fixtures.ts
@@ -0,0 +1,14 @@
+import { Locale } from "@types";
+
+export const localesOptions = [
+  { localeCode: Locale.EN, localeName: "English" },
+  { localeCode: Locale.PL, localeName: "polski" },
+];
+
+export const preferencesEN = {
+  locale: Locale.EN,
+};
+
+export const preferencesPL = {
+  locale: Locale.PL,
+};
