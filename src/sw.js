8400b4cc9668f59a160b99ac71163430ae439b45
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2020-08-06 15:21:58 +0200

    Do not cache csv and pdf in service worker (#838)
    * Do not cache csv and pdf in service worker
    
    * Update router to ignore media path
    
    * Exclude media files from sw routing

--- src/sw.js
+++ src/sw.js
@@ -10,13 +10,17 @@ workbox.core.setCacheNameDetails({
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 
 workbox.routing.registerRoute(
-  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg)"),
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg|csv|pdf)"),
   new workbox.strategies.NetworkFirst()
 );
 
 workbox.routing.registerNavigationRoute(
   workbox.precaching.getCacheKeyForURL("/index.html"),
   {
-    blacklist: [new RegExp("/graphql"), new RegExp("/dashboard")],
+    blacklist: [
+      new RegExp("/graphql"),
+      new RegExp("/dashboard"),
+      new RegExp("/media/export_files"),
+    ],
   }
 );
