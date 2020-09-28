9a8aca53f10759422d04cd29f88379f7c652ee73
Magdalena Markusik <magdalena.markusik@mirumee.com>
2020-09-28 13:51:49 +0200

    wip

--- src/@next/components/organisms/DummyPaymentGateway/DummyPaymentGateway.tsx
+++ src/@next/components/organisms/DummyPaymentGateway/DummyPaymentGateway.tsx
@@ -1,16 +1,25 @@
 import { Formik } from "formik";
 import React from "react";
 
-import { Radio } from "@components/atoms";
-
 import * as S from "./styles";
 import { IProps } from "./types";
+import { TextField } from "@components/molecules";
+import { useIntl } from "react-intl";
+import * as SI from "src/@next/components/atoms/Input/styles";
+import { Input } from "@components/atoms";
 
 export const statuses = [
   { token: "charged", label: "Charged" },
   { token: "fully-refunded", label: "Fully refunded" },
   { token: "not-charged", label: "Not charged" },
 ];
+
+const initialValues = {
+  nameOnCard: "",
+  cardNumber: "",
+  cvc: "",
+  expirationDate: "",
+};
 
 /**
  * Dummy payment gateway.
@@ -21,9 +30,13 @@ const DummyPaymentGateway: React.FC<IProps> = ({
   formId,
   initialStatus,
 }: IProps) => {
+  const intl = useIntl();
   return (
     <Formik
-      initialValues={{ status: initialStatus || statuses[0].token }}
+      initialValues={{
+        ...initialValues,
+        status: initialStatus || statuses[0].token,
+      }}
       onSubmit={(values, { setSubmitting }) => {
         processPayment(values.status);
         setSubmitting(false);
@@ -43,22 +56,28 @@ const DummyPaymentGateway: React.FC<IProps> = ({
           onSubmit={handleSubmit}
           data-test="dummyPaymentGatewayForm"
         >
-          {statuses.map(({ token, label }) => {
-            return (
-              <S.Status key={token}>
-                <Radio
-                  key={token}
-                  type="radio"
-                  name="status"
-                  value={token}
-                  checked={values.status === token}
-                  onChange={handleChange}
-                >
-                  <span>{label}</span>
-                </Radio>
-              </S.Status>
-            );
-          })}
+          <TextField
+            name="nameOnCard"
+            label={intl.formatMessage({ defaultMessage: "Name on Card" })}
+            value={values.nameOnCard}
+          />
+          <SI.Wrapper
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "stretch",
+            }}
+          >
+            <Input
+              name="cardNumber"
+              label={intl.formatMessage({ defaultMessage: "Card number" })}
+              value={values.cardNumber}
+              // {...rest}
+              // error={hasErrors}
+            />
+            <input type="text" name="cvc" value={values.cvc} />
+            <input name="cardNumber" value={values.cardNumber} />
+          </SI.Wrapper>
         </S.Form>
       )}
     </Formik>
--- src/@next/components/organisms/DummyPaymentGateway/styles.ts
+++ src/@next/components/organisms/DummyPaymentGateway/styles.ts
@@ -1,3 +1,4 @@
+import { IProps } from "@components/atoms/Icon/types";
 import { styled } from "@styles";
 
 export const Form = styled.form``;
@@ -6,3 +7,19 @@ export const Status = styled.div`
   display: block;
   margin: 18px;
 `;
+
+export const BareInput = styled.input<IProps>`
+  padding: 0.8rem 1rem;
+  margin: 0;
+  border: none;
+  width: 100%;
+  font-size: ${props => props.theme.typography.baseFontSize};
+  outline: none;
+`;
+
+//background-color: transparent;
+// &:-webkit-autofill {
+//   & + label {
+//     ${props => activeLabelStyles(props.theme, props.labelBackground)};
+//   }
+// }
