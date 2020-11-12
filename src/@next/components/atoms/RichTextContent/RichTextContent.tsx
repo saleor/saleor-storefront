1fcc201cfb2e5eda25d91c7d6a54eaaf27d989f0
Maciej Korycinski <korycinski.maciej@gmail.com>
2020-11-12 11:51:12 +0100

    Update RichTextContent.tsx
    Force CI to rebuild storefront with new backend

--- src/@next/components/atoms/RichTextContent/RichTextContent.tsx
+++ src/@next/components/atoms/RichTextContent/RichTextContent.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-danger */
 
-//  import { sanitize } from "dompurify";
+// import { sanitize } from "dompurify";
 // import draftToHtml from "draftjs-to-html";
 import React from "react";
 
