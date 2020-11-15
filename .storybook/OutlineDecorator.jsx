f7b2c9698c7eb378fe8da296cc5321d3993a27f1
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2020-11-15 12:47:55 +0100

    Upgrade storybook to 6.0

--- .storybook/OutlineDecorator.jsx
+++ .storybook/OutlineDecorator.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import { GlobalStyle } from "../src/@next/globalStyles";
+import { defaultTheme } from "../src/@next/globalStyles";
 import * as S from "./styles";
 
-export const OutLineDecorator = storyFn => (
-  <S.Wrapper>
-    {storyFn()}
-    <S.Outline />
-    <GlobalStyle />
-  </S.Wrapper>
+export const OutLineDecorator = Story => (
+  <ThemeProvider theme={defaultTheme}>
+    <S.Wrapper>
+      <Story />
+      <S.Outline />
+      <GlobalStyle />
+    </S.Wrapper>
+  </ThemeProvider>
 );
