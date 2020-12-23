379fc1c2283d464ceb965c6abd22404951c58a17
Yan <wangyan6783@gmail.com>
2020-12-23 15:20:55 -0500

    authorize.net config updates

--- src/@next/components/organisms/AuthorizeNetPaymentGateway/types.ts
+++ src/@next/components/organisms/AuthorizeNetPaymentGateway/types.ts
@@ -2,5 +2,9 @@ export interface IProps {
   /**
    * Method called after the form is submitted. Passed token attribute will be used to create payment.
    */
+  config: {
+    field: string;
+    value: string;
+  }[];
   processPayment: (token: string) => void;
 }
