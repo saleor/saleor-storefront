aa1e8bc86252fd1f4fa6b9f9a0f8436ed9cecd71
trungle <thanhtrungnait88@gmail.com>
2021-04-06 19:59:44 -0700

    fix eslint footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import { SocialMediaIcon } from "..";
 // import Nav from "./Nav";
 import Email from "./Email";
 import SiteMap from "./SiteMap";
+
 import "./scss/index.scss";
 
 const Footer: React.FC = () => (
@@ -15,14 +16,18 @@ const Footer: React.FC = () => (
       <div className="footer__favicons">
         <div className="favicons-col">
           <div className="social-main">
-          <span className="sociality-text">Download:</span>
-          <a href="#" className="app-store">Available on the App Store</a>
-          <a href="#" className="android">Available on Android</a>
+            <span className="sociality-text">Download:</span>
+            <a href="#" className="app-store">
+              Available on the App Store
+            </a>
+            <a href="#" className="android">
+              Available on Android
+            </a>
           </div>
         </div>
         <div className="favicons-col">
           <div className="favicons-col-main">
-          <span className="sociality-text">Follow Us:</span>
+            <span className="sociality-text">Follow Us:</span>
             {SOCIAL_MEDIA.map(medium => (
               <SocialMediaIcon medium={medium} key={medium.ariaLabel} />
             ))}
