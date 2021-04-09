2203559dcee2accd9503110c18474c0f3c5747fd
thanghoian123 <kuthang32@gmail.com>
2021-04-09 15:46:34 +0700

    feature/12324-integration(fix mobile UI)

--- src/@next/components/organisms/ProductList/ProductList.tsx
+++ src/@next/components/organisms/ProductList/ProductList.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { FormattedMessage } from "react-intl";
 
 import { Button, Loader } from "@components/atoms";
 import { ProductTile } from "@components/molecules";
 
-import Pagination from "../../../../components/Pagination";
 import { generateProductUrl } from "../../../../core/utils";
 import * as S from "./styles";
 import { IProps } from "./types";
@@ -17,10 +16,6 @@ export const ProductList: React.FC<IProps> = ({
   testingContextId,
   onLoadMore = () => null,
 }: IProps) => {
-  const [paging, setPaging] = useState({ curPage: 1, pageSize: 10 });
-  const onChange = (curPage: number, pageSize: number) => {
-    setPaging({ ...paging, pageSize, curPage });
-  };
   return (
     <S.Wrapper>
       <S.List data-test="productList" data-test-id={testingContextId}>
@@ -40,12 +35,6 @@ export const ProductList: React.FC<IProps> = ({
           );
         })}
       </S.List>
-      <Pagination
-        onChange={onChange}
-        pageSize={10}
-        totalPage={15}
-        pageDisplay={5}
-      />
       <S.Loader>
         {loading ? (
           <Loader />
