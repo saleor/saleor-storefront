fb2cf81f1f77f1f2e6bb34bf837951f6a1c4f6dd
Nguyen Van Thien <thien.nguyenvan3.ncc@gmail.com>
2021-04-09 11:04:57 +0700

    fix eslin file AddNewTile

--- src/@next/components/atoms/AddNewTile/AddNewTile.tsx
+++ src/@next/components/atoms/AddNewTile/AddNewTile.tsx
@@ -4,8 +4,12 @@ import { Icon } from "../Icon";
 import { Tile } from "../Tile";
 import * as S from "./styles";
 import { IProps } from "./types";
+
+
+
  
  
+
 export const AddNewTile: React.FC<IProps> = ({ type, ...props }: IProps) => {
   return (
     <Tile tileType="addNew" {...props}>
