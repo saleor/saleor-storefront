e0ae2a0553dce3fc56fd261a7ddb31fa5766b020
Nguyen Ngoc Duy <duy.nguyenngoc@ncc.asia>
2021-04-09 01:03:41 -0700

    lint

--- src/@next/components/atoms/AddNewTile/test.tsx
+++ src/@next/components/atoms/AddNewTile/test.tsx
@@ -1,9 +1,10 @@
-import "jest-styled-components";
-
+/* eslint-disable no-undef */
 import { mount } from "enzyme";
+import "jest-styled-components";
 import React from "react";
-
 import { AddNewTile } from ".";
+
+
 
 describe("<AddNewTile />", () => {
   it("should display child component with card type passed as prop and rendered inside it as a text", () => {
--- src/@next/components/atoms/AddNewTile/types.ts
+++ src/@next/components/atoms/AddNewTile/types.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/interface-name-prefix
 export interface IProps {
   type: string;
   onClick?: () => void;
