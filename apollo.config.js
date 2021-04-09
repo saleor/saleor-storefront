c79c2b04e8f26a37f0b152d1cecce1b5f7d55310
nguyenngocduyby96 <43125137+nguyenngocduyby96@users.noreply.github.com>
2021-04-09 01:08:58 -0700

    Delete apollo.config.js

--- apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    excludes: ["**/__tests__/**/*", "**/@sdk/**/*"],
-    service: {
-      name: "saleor",
-      url: "http://172.16.11.19:8080/graphql/",
-    },
-  },
-};
