b91ae4635e637f0a66358c332df3958046074217
Nguyen Van Thien <thien.nguyenvan3.ncc@gmail.com>
2021-04-14 17:36:31 +0700

    tweak

--- src/@next/components/molecules/AccountTabTiles/InforProfile.tsx
+++ src/@next/components/molecules/AccountTabTiles/InforProfile.tsx
@@ -1,94 +1,90 @@
-import { Tile } from "@components/atoms";
 import React from "react";
-import "./css/styles.scss";
+
+import { Tile } from "@components/atoms";
+
 import * as S from "./styles";
 
+import "./css/styles.scss";
+
 export const InforProfile = () => {
-    return(
-        <div>
-            <Tile>
-                <S.Layout>
-                <div style={{flex: 1}}>
-                    <S.Header>
-                        Personal Information
-                    </S.Header>
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            My Profile
-                        </a>
-                    </p>
-                    
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Member Profile
-                        </a>
-                    </p>
-                   
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Upload My Photo
-                        </a>
-                    </p>
-                    
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Privacy Setting
-                        </a>
-                    </p>
-                    
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Email Services
-                        </a>
-                    </p>
-                </div>
-                <div style={{flex: 1}}>
-                    <S.Header>
-                        Account Security
-                    </S.Header>
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Change Email Address
-                        </a>
-                    </p>
-                    
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Change Password
-                        </a>
-                    </p>
-                    
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Set Security Question
-                        </a>
-                    </p>
-                   
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Manage Verification Phones
-                        </a>
-                    </p>
-                    
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            Manage My Connected Accounts
-                        </a>
-                    </p>
-                </div>
-                <div style={{flex: 1}}>
-                    <S.Header>
-                        Finance Account
-                    </S.Header>
-                    <p>
-                        <a href="#" className="linkInforProfile">
-                            My Transactions Center
-                        </a>
-                    </p>
-                </div>
-                </S.Layout>
-                
-            </Tile>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Tile>
+        <S.Layout>
+          <div style={{ flex: 1 }}>
+            <S.Header>Personal Information</S.Header>
+            <p>
+              <a href="1" className="linkInforProfile">
+                My Profile
+              </a>
+            </p>
+
+            <p>
+              <a href="2" className="linkInforProfile">
+                Member Profile
+              </a>
+            </p>
+
+            <p>
+              <a href="3" className="linkInforProfile">
+                Upload My Photo
+              </a>
+            </p>
+
+            <p>
+              <a href="4" className="linkInforProfile">
+                Privacy Setting
+              </a>
+            </p>
+
+            <p>
+              <a href="5" className="linkInforProfile">
+                Email Services
+              </a>
+            </p>
+          </div>
+          <div style={{ flex: 1 }}>
+            <S.Header>Account Security</S.Header>
+            <p>
+              <a href="6" className="linkInforProfile">
+                Change Email Address
+              </a>
+            </p>
+
+            <p>
+              <a href="7" className="linkInforProfile">
+                Change Password
+              </a>
+            </p>
+
+            <p>
+              <a href="8" className="linkInforProfile">
+                Set Security Question
+              </a>
+            </p>
+
+            <p>
+              <a href="9" className="linkInforProfile">
+                Manage Verification Phones
+              </a>
+            </p>
+
+            <p>
+              <a href="10" className="linkInforProfile">
+                Manage My Connected Accounts
+              </a>
+            </p>
+          </div>
+          <div style={{ flex: 1 }}>
+            <S.Header>Finance Account</S.Header>
+            <p>
+              <a href="11" className="linkInforProfile">
+                My Transactions Center
+              </a>
+            </p>
+          </div>
+        </S.Layout>
+      </Tile>
+    </div>
+  );
+};
--- src/@next/components/molecules/AccountTabTiles/ProfileUser.tsx
+++ src/@next/components/molecules/AccountTabTiles/ProfileUser.tsx
@@ -1,56 +1,47 @@
-import { Tile } from "@components/atoms";
 import { useAuth } from "@saleor/sdk";
 import React from "react";
-import "./css/styles.scss";
+
+import { Tile } from "@components/atoms";
+
 import * as S from "./styles";
 
+import "./css/styles.scss";
+
 export const ProfileUser: React.FC = () => {
-    const { user } = useAuth();
+  const { user } = useAuth();
 
-   return(
-       <S.TileWrapper>
-           <Tile>
-           <S.Layout>
-               <div style={{flex:1}}>
-                    <a href="#" className="linkProfileUser">
-                        Upload Photo
-                    </a>
-               </div>
-               
-               <div style={{flex:3}}>
-                    <p>
-                        Your Member ID: {user?.id}
-                    </p>
-                    <p>
-                        Email: {user?.email}
-                    </p>
-                    <p>
-                        Linked Mobile:
-                        <a href="#" className="linkProfileUser" style={{marginLeft: "5px"}}>
-                            Enter Mobile Number
-                        </a>
-                    </p>
-               </div>
+  return (
+    <S.TileWrapper>
+      <Tile>
+        <S.Layout>
+          <div style={{ flex: 1 }}>
+            <a href="1" className="linkProfileUser">
+              Upload Photo
+            </a>
+          </div>
 
-               {/* <div style={{flex:1}}>
-                    <p>
-                        
-                    </p>
-                    <p>
-                        
-                    </p>
-                    <p>
-                        
-                    </p>
-               </div> */}
-               
-               <div style={{flex:3}}>
-                    <a href="#" className="linkProfileUser">
-                        Change Email Address
-                    </a>
-               </div>
-           </S.Layout>
-           </Tile>
-       </S.TileWrapper>
-   )
-}
+          <div style={{ flex: 3 }}>
+            <p>Your Member ID: {user?.id}</p>
+            <p>Email: {user?.email}</p>
+            <p>
+              Linked Mobile:
+              <a
+                href="2"
+                className="linkProfileUser"
+                style={{ marginLeft: "5px" }}
+              >
+                Enter Mobile Number
+              </a>
+            </p>
+          </div>
+
+          <div style={{ flex: 3 }}>
+            <a href="3" className="linkProfileUser">
+              Change Email Address
+            </a>
+          </div>
+        </S.Layout>
+      </Tile>
+    </S.TileWrapper>
+  );
+};
