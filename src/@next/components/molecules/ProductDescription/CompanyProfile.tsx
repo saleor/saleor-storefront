5b8ddf27948382792325711edb01b68ac6733eee
Nguyen Ngoc Duy <duy.nguyenngoc@ncc.asia>
2021-05-17 15:59:21 +0700

    add map to company profile section

--- src/@next/components/molecules/ProductDescription/CompanyProfile.tsx
+++ src/@next/components/molecules/ProductDescription/CompanyProfile.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import { GoogleMap, withGoogleMap, withScriptjs } from "react-google-maps";
+import Marker from "react-google-maps/lib/components/Marker";
 import { FormattedMessage } from "react-intl";
 
 import { paths } from "@paths";
@@ -9,7 +11,25 @@ import { IProps } from "./types";
 
 export const CompanyProfile: React.FC<IProps> = ({ store }: IProps) => {
   const tempDescription =
-    store?.description && store.description.replace(/'/g, '"');
+    store?.description && store?.description.replace(/'/g, '"');
+
+  const isShowMap = store?.latlong && store?.latlong !== "";
+
+  const lat = store?.latlong ? parseFloat(store.latlong.split(",")[0]) : 0;
+  const lng = store?.latlong ? parseFloat(store?.latlong?.split(",")[1]) : 0;
+
+  const position = {
+    lat,
+    lng,
+  };
+
+  const Map = () => (
+    <GoogleMap defaultZoom={15} defaultCenter={position}>
+      <Marker position={position} />
+    </GoogleMap>
+  );
+
+  const WrappedMap = withScriptjs<any>(withGoogleMap(Map));
   return (
     <S.Wrapper>
       <S.WrapBox>
@@ -26,13 +46,7 @@ export const CompanyProfile: React.FC<IProps> = ({ store }: IProps) => {
                   <FormattedMessage defaultMessage="Name" />
                 </S.CompanyInfoTd>
                 <S.CompanyInfoTd>
-                  {store ? (
-                    <>
-                      <S.AttributeName>{store.name}</S.AttributeName>{" "}
-                    </>
-                  ) : (
-                    "-"
-                  )}
+                  <S.AttributeName>{store?.name || "-"}</S.AttributeName>
                 </S.CompanyInfoTd>
                 <S.CompanyInfoTd>
                   <FormattedMessage defaultMessage="Description" />
@@ -40,11 +54,9 @@ export const CompanyProfile: React.FC<IProps> = ({ store }: IProps) => {
                 <S.CompanyInfoTd>
                   {tempDescription &&
                   JSON.parse(tempDescription).description ? (
-                    <>
-                      <S.AttributeName>
-                        {JSON.parse(tempDescription).description}
-                      </S.AttributeName>
-                    </>
+                    <S.AttributeName>
+                      {JSON.parse(tempDescription).description}
+                    </S.AttributeName>
                   ) : (
                     "-"
                   )}
@@ -55,54 +67,30 @@ export const CompanyProfile: React.FC<IProps> = ({ store }: IProps) => {
                   <FormattedMessage defaultMessage="Phone" />
                 </S.CompanyInfoTd>
                 <S.CompanyInfoTd>
-                  {store ? (
-                    <>
-                      <S.AttributeName>{store.phone}</S.AttributeName>{" "}
-                    </>
-                  ) : (
-                    "-"
-                  )}
+                  <S.AttributeName>{store?.phone || "-"}</S.AttributeName>{" "}
                 </S.CompanyInfoTd>
                 <S.CompanyInfoTd>
-                  <FormattedMessage defaultMessage="Coordinates" />
+                  <FormattedMessage defaultMessage="Acreage" />
                 </S.CompanyInfoTd>
                 <S.CompanyInfoTd>
-                  {store ? (
-                    <>
-                      <S.AttributeName>{store.latlong}</S.AttributeName>{" "}
-                    </>
-                  ) : (
-                    "-"
-                  )}
-                </S.CompanyInfoTd>
-              </S.CompanyInfoTr>
-              <S.CompanyInfoTr>
-                <S.CompanyInfoTd>
-                  <FormattedMessage defaultMessage="acreage" />
-                </S.CompanyInfoTd>
-                <S.CompanyInfoTd>
-                  {store ? (
-                    <>
-                      <S.AttributeName>{store.acreage}</S.AttributeName>{" "}
-                    </>
-                  ) : (
-                    "-"
-                  )}
+                  <S.AttributeName>{store?.acreage || "-"}</S.AttributeName>
                 </S.CompanyInfoTd>
               </S.CompanyInfoTr>
             </S.CompanyInfoBody>
           </S.CompanyInfo>
-          {/* <S.CompanyLocation>
-              <S.SubTitleText>
-                <FormattedMessage defaultMessage="Location" />
-              </S.SubTitleText>
-            </S.CompanyLocation>
-            <S.CompanyLocation>
-              <S.SubTitleText>
-                <FormattedMessage defaultMessage="Images" />
-              </S.SubTitleText>
-            </S.CompanyLocation> */}
         </S.CompanyWrapInfo>
+
+        {isShowMap && (
+          <div style={{ margin: "1rem 0" }}>
+            <WrappedMap
+              name="latlong"
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAe38lpcvEH7pLWIbgNUPNHsPnyIYwkc60&v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ width: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </div>
+        )}
 
         {store?.id && (
           <div style={{ display: "flex", justifyContent: "center" }}>
