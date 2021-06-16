6c2322ff21bf20c3ab2f34211013733499f990bf
sektordv <roman@sawblade.pl>
2021-06-16 18:12:34 +0200

    added apolloClient to window based on env

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { useAuth } from "@saleor/sdk";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Loader } from "@components/atoms";
 import { useDynamicRouteRedirect } from "@hooks";
 import { demoMode } from "@temp/constants";
-import { ShopConfig } from "@utils/ssr";
+import { getSaleorApi, ShopConfig } from "@utils/ssr";
 
 import {
   Footer,
@@ -21,6 +21,12 @@ import "../globalStyles/scss/index.scss";
 
 type AppProps = ShopConfig;
 
+declare global {
+  interface Window {
+    __APOLLO_CLIENT__: any;
+  }
+}
+
 const App: React.FC<AppProps> = ({
   footer,
   mainMenu,
@@ -31,6 +37,15 @@ const App: React.FC<AppProps> = ({
   const willRedirect = useDynamicRouteRedirect();
   const { tokenRefreshing, tokenVerifying } = useAuth();
   const loading = tokenRefreshing || tokenVerifying || willRedirect;
+
+  useEffect(() => {
+    const attachClient = async () => {
+      const { apolloClient } = await getSaleorApi();
+      if (window) window.__APOLLO_CLIENT__ = apolloClient;
+    };
+    if (process.env.NEXT_PUBLIC_ENABLE_APOLLO_DEVTOOLS === "true")
+      attachClient();
+  }, []);
 
   return (
     <ShopProvider shopConfig={shopConfig}>
