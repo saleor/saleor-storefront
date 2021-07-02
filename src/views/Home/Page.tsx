2bee0c64360a16d8f8fc9273fd0e33a0b2ce0fc0
namnguyenvan2k <nam.nguyenvan.ncc@gmail.com>
2021-07-02 10:01:00 +0700

    update carousel homepage

--- src/views/Home/Page.tsx
+++ src/views/Home/Page.tsx
@@ -1,12 +1,14 @@
-import classNames from "classnames";
-import Link from "next/link";
+// import Link from "next/link";
+import Carousel from "nuka-carousel";
 import * as React from "react";
-import { FormattedMessage, useIntl } from "react-intl";
+import { /* FormattedMessage, */ useIntl } from "react-intl";
 
-import { Button, Loader, ProductsFeatured } from "../../components";
+import { /* Button, Loader, */ ProductsFeatured } from "../../components";
 import { structuredData } from "../../core/SEO/Homepage/structuredData";
-import { generateCategoryUrl } from "../../core/utils";
-import noPhotoImg from "../../images/no-photo.svg";
+// import { generateCategoryUrl } from "../../core/utils";
+import nextCarouselImg from "../../images/nextCarouselHomePage.svg";
+// import noPhotoImg from "../../images/no-photo.svg";
+import preCarouselImg from "../../images/preCarouselHomePage.svg";
 import {
   ProductsList_categories,
   ProductsList_collection_backgroundImage,
@@ -33,17 +35,57 @@ const Page: React.FC<{
       </script>
       <div
         className="home-page__hero"
-        style={
-          backgroundImage
-            ? { backgroundImage: `url(${backgroundImage.url})` }
-            : null
-        }
+        // style={
+        //   backgroundImage
+        //     ? { backgroundImage: `url(${backgroundImage.url})` }
+        //     : null
+        // }
       >
+        <Carousel
+          speed={1000}
+          renderCenterLeftControls={({ previousSlide }) => (
+            <button
+              style={{ transform: "translateX(-22px)" }}
+              onClick={previousSlide}
+            >
+              <img src={preCarouselImg} alt="" />
+            </button>
+          )}
+          renderCenterRightControls={({ nextSlide }) => (
+            <button
+              style={{ transform: "translateX(22px)" }}
+              onClick={nextSlide}
+            >
+              <img src={nextCarouselImg} alt="" />
+            </button>
+          )}
+        >
+          <img
+            src="https://anhnendep.net/wp-content/uploads/2015/07/hinh-nen-trai-cay-dep-7.jpg"
+            alt=""
+          />
+          <img
+            src="https://media.vov.vn/sites/default/files/styles/large/public/2020-10/nhung-loai-trai-cay-khong-nen-an-khi-giam-can-590.jpg"
+            alt=""
+          />
+          <img
+            src="https://2.bp.blogspot.com/-0LVDww5gZw4/VpHOCzNCLeI/AAAAAAAAIcg/Z2I17dbzy3A/s1600/Hinh-anh-dep-cua-trai-cay-hoa-qua-xanh-sach%2B%25281%2529.jpg"
+            alt=""
+          />
+          <img
+            src="https://anhnendep.net/wp-content/uploads/2016/02/hinh-nen-hoa-qua-trai-cay-dep-2016-05.jpg"
+            alt=""
+          />
+          <img
+            src="https://anhnendep.net/wp-content/uploads/2015/07/hinh-nen-trai-cay-dep-13.jpg"
+            alt=""
+          />
+        </Carousel>
         <div className="home-page__hero-text">
-          <div>
+          {/* <div>
             <span className="home-page__hero__title">
               <h1>
-                <FormattedMessage defaultMessage="Final reduction" />
+                <FormattedMessage defaultMessage="Final reduction" />/
               </h1>
             </span>
           </div>
@@ -53,9 +95,9 @@ const Page: React.FC<{
                 <FormattedMessage defaultMessage="Up to 70% off sale" />
               </h1>
             </span>
-          </div>
+          </div> */}
         </div>
-        <div className="home-page__hero-action">
+        {/* <div className="home-page__hero-action">
           {loading && !categories ? (
             <Loader />
           ) : (
@@ -74,49 +116,9 @@ const Page: React.FC<{
               </Link>
             )
           )}
-        </div>
+        </div> */}
       </div>
-      {categoriesExist() && (
-        <div className="home-page__categories">
-          <div className="container">
-            {/* <h3>
-              <FormattedMessage defaultMessage="Shop by category" />
-            </h3> */}
-            <div className="home-page__categories__list">
-              {categories.edges.map(({ node: category }) => (
-                <div
-                  key={category.id}
-                  className="home-page__categories__list__item"
-                >
-                  <Link
-                    href={generateCategoryUrl(category.id, category.name)}
-                    key={category.id}
-                  >
-                    <a>
-                      <h3>{category.name}</h3>
-                      <div
-                        className={classNames(
-                          "home-page__categories__list__image",
-                          {
-                            "home-page__categories__list__image--no-photo": !category.backgroundImage,
-                          }
-                        )}
-                        style={{
-                          backgroundImage: `url(${
-                            category.backgroundImage
-                              ? category.backgroundImage.url
-                              : noPhotoImg
-                          })`,
-                        }}
-                      />
-                    </a>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
+      {}
       <ProductsFeatured
         title={intl.formatMessage({ defaultMessage: "Featured" })}
       />
