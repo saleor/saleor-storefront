ac803a8a86909cf81dde695cb1e4205cfd779fdc
Jakub Majorek <majorek.jakub@gmail.com>
2021-09-10 14:26:41 +0200

    Add security headers (#1096)

--- next.config.js
+++ next.config.js
@@ -8,11 +8,34 @@ const withBaseConfig = require("./config/next/config.base");
 const withDevConfig = require("./config/next/config.dev");
 const withProdConfig = require("./config/next/config.prod");
 
-module.exports = withPlugins([
-  [withOptimizedImages, { handleImages: ["jpeg", "png", "webp", "gif"] }],
-  withTM,
-  withBaseConfig,
-  withServiceWorkerConfig,
-  [withDevConfig, {}, [PHASE_DEVELOPMENT_SERVER]],
-  [withProdConfig, {}, ["!" + PHASE_DEVELOPMENT_SERVER]],
-]);
+module.exports = withPlugins(
+  [
+    [withOptimizedImages, { handleImages: ["jpeg", "png", "webp", "gif"] }],
+    withTM,
+    withBaseConfig,
+    withServiceWorkerConfig,
+    [withDevConfig, {}, [PHASE_DEVELOPMENT_SERVER]],
+    [withProdConfig, {}, ["!" + PHASE_DEVELOPMENT_SERVER]],
+  ],
+  {
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            {
+              key: "x-content-type-options",
+              value: "nosniff",
+            },
+            { key: "x-xss-protection", value: "1" },
+            { key: "x-frame-options", value: "DENY" },
+            {
+              key: "strict-transport-security",
+              value: "max-age=31536000; includeSubDomains",
+            },
+          ],
+        },
+      ];
+    },
+  }
+);
